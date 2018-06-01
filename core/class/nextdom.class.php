4d5fc7d35e054377b3e58da13bf8cf21f1b158fd
luc <4697568+ColonelMoutarde@users.noreply.github.com>
2018-06-01 20:17:33 +0200

    Ajout de la licence sur nextdom.class.php

--- core/class/nextdom.class.php
+++ core/class/nextdom.class.php
@@ -1,5 +1,21 @@
 <?php
 
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* This file is part of NextDom.
  *
  * NextDom is free software: you can redistribute it and/or modify
@@ -15,6 +31,7 @@
  * You should have received a copy of the GNU General Public License
  * along with NextDom. If not, see <http://www.gnu.org/licenses/>.
  */
+/* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../core/php/core.inc.php';
 global $NEXTDOM_INTERNAL_CONFIG;
 class nextdom {
