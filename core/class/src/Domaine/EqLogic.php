71acf6084147441cd018632aaf18229c8ea018f0
lucsanchez <lucsanchez@192.168.1.123>
2018-06-10 12:42:23 +0200

    Ajout des getter et setter à EqLogic

--- core/class/src/Domaine/EqLogic.php
+++ core/class/src/Domaine/EqLogic.php
@@ -19,5 +19,277 @@ namespace NextDom\src\Domaine;
 
 class EqLogic
 {
-    //put your code here
+
+    /**
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     *
+     * @var string
+     */
+    private $genericType;
+
+    /**
+     *
+     * @var string
+     */
+    private $logicalId;
+
+    /**
+     *
+     * @var int
+     */
+    private $objectId;
+
+    /**
+     *
+     * @var string
+     */
+    private $eqTypeName;
+
+    /**
+     *
+     * @var string
+     */
+    private $configuration;
+
+    /**
+     *
+     * @var bool 
+     */
+    private $isVisible;
+
+    /**
+     *
+     * @var int 
+     */
+    private $eqRealId;
+
+    /**
+     *
+     * @var int
+     */
+    private $isEnable;
+
+    /**
+     *
+     * @var string
+     */
+    private $status;
+
+    /**
+     *
+     * @var int 
+     */
+    private $timeout;
+
+    /**
+     *
+     * @var string
+     */
+    private $category;
+
+    /**
+     *
+     * @var string
+     */
+    private $display;
+
+    /**
+     *
+     * @var int
+     */
+    private $order;
+
+    /**
+     *
+     * @var string
+     */
+    private $comment;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getGenericType()
+    {
+        return $this->genericType;
+    }
+
+    public function getLogicalId()
+    {
+        return $this->logicalId;
+    }
+
+    public function getObjectId()
+    {
+        return $this->objectId;
+    }
+
+    public function getEqTypeName()
+    {
+        return $this->eqTypeName;
+    }
+
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+
+    public function getIsVisible()
+    {
+        return $this->isVisible;
+    }
+
+    public function getEqRealId()
+    {
+        return $this->eqRealId;
+    }
+
+    public function getIsEnable()
+    {
+        return $this->isEnable;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function getTimeout()
+    {
+        return $this->timeout;
+    }
+
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    public function getDisplay()
+    {
+        return $this->display;
+    }
+
+    public function getOrder()
+    {
+        return $this->order;
+    }
+
+    public function getComment()
+    {
+        return $this->comment;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function setGenericType($genericType)
+    {
+        $this->genericType = $genericType;
+        return $this;
+    }
+
+    public function setLogicalId($logicalId)
+    {
+        $this->logicalId = $logicalId;
+        return $this;
+    }
+
+    public function setObjectId($objectId)
+    {
+        $this->objectId = $objectId;
+        return $this;
+    }
+
+    public function setEqTypeName($eqTypeName)
+    {
+        $this->eqTypeName = $eqTypeName;
+        return $this;
+    }
+
+    public function setConfiguration($configuration)
+    {
+        $this->configuration = $configuration;
+        return $this;
+    }
+
+    public function setIsVisible($isVisible)
+    {
+        $this->isVisible = $isVisible;
+        return $this;
+    }
+
+    public function setEqRealId($eqRealId)
+    {
+        $this->eqRealId = $eqRealId;
+        return $this;
+    }
+
+    public function setIsEnable($isEnable)
+    {
+        $this->isEnable = $isEnable;
+        return $this;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+        return $this;
+    }
+
+    public function setTimeout($timeout)
+    {
+        $this->timeout = $timeout;
+        return $this;
+    }
+
+    public function setCategory($category)
+    {
+        $this->category = $category;
+        return $this;
+    }
+
+    public function setDisplay($display)
+    {
+        $this->display = $display;
+        return $this;
+    }
+
+    public function setOrder($order)
+    {
+        $this->order = $order;
+        return $this;
+    }
+
+    public function setComment($comment)
+    {
+        $this->comment = $comment;
+        return $this;
+    }
+
 }
