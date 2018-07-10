ac21aabd8057198169bd6d59bd09bda3ead37a65
luc <4697568+ColonelMoutarde@users.noreply.github.com>
2018-07-10 15:11:09 +0200

    Add Interface DBInterface

---
+++ core/src/Interfaces/DBInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the NextDom software (https://github.com/NextDom or http://nextdom.github.io).
+ * Copyright (c) 2018 NextDom -- ColonelMoutarde.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace NextDom\Interfaces;
+ 
+interface DBInterface
+{
+    function isDataTableExists(): bool;
+    
+    function createDataTable();
+    
+    function dropDataTable();
+    
+    function deleteData(string $code);
+    
+    function isDataExists(string $code): bool;   
+    
+}
