5b7bc9e8ac735170b29c3762c41022346dc0b36a
Unknown <vincent.fresnel@gmail.com>
2018-07-11 20:26:54 +0200

    debug

--- core/src/Helpers/PrepareView.php
+++ core/src/Helpers/PrepareView.php
@@ -111,7 +111,6 @@ class PrepareView
      * @return string Code HTML du message d'erreur
      */
     public static function showAlertMessage(string $msg) {
-        notify("Erreur", $msg, 'error');
         echo '<div class="alert alert-danger">'.$msg.'</div>';
     }
 
