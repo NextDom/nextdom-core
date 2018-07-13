094d1f1d3f20b6bdf364b44bbae98a16e962391e
Unknown <vincent.fresnel@gmail.com>
2018-07-13 10:58:47 +0200

    divers

--- desktop/template/menu.php
+++ desktop/template/menu.php
@@ -34,7 +34,7 @@ $panelMenu = PrepareView::getPanelMenu();
                 <span class="icon-bar"></span>
             </button>
         </div>
-        <nav class="navbar-collapse collapse">
+        <nav class="navbar-collapse collapse text-center">
             <div class="collapse navbar-collapse navbar-left col-lg-4 col-md-4" style="margin-left:25px">
                 <ul class="nav navbar-nav">
                     <li class="dropdown cursor">
@@ -52,7 +52,7 @@ $panelMenu = PrepareView::getPanelMenu();
                                     </ul>
                                 </li>
                                 <li class="dropdown-submenu">
-                                    <a data-toggle="dropdown" id="bt_gotoView"><i class="fa fa-picture-o"></i>{{Vue}}</a>
+                                    <a data-toggle="dropdown" id="bt_gotoView"><i class="fa fa-picture-o"></i> {{Vue}}</a>
                                     <ul class="dropdown-menu">
                                         <?php
                                         foreach (view::all() as $view_menu) {
@@ -62,7 +62,7 @@ $panelMenu = PrepareView::getPanelMenu();
                                     </ul>
                                 </li>
                                 <li class="dropdown-submenu">
-                                    <a data-toggle="dropdown" id="bt_gotoPlan"><i class="fa fa-paint-brush"></i>{{Design}}</a>
+                                    <a data-toggle="dropdown" id="bt_gotoPlan"><i class="fa fa-paint-brush"></i> {{Design}}</a>
                                     <ul class="dropdown-menu">
                                         <?php
                                         foreach (planHeader::all() as $plan_menu) {
@@ -79,11 +79,11 @@ $panelMenu = PrepareView::getPanelMenu();
                         <li class="dropdown cursor">
                             <a data-toggle="dropdown"><i class="fa fa-stethoscope fa-3x"></i><br><span class="hidden-xs hidden-sm hidden-md">{{Analyse}}</span> <b class="caret"></b></a>
                             <ul class="dropdown-menu" role="menu">
-                                <li><a href="index.php?v=d&p=history"><i class="fa fa-bar-chart-o"></i>{{Historique}}</a></li>
+                                <li><a href="index.php?v=d&p=history"><i class="fa fa-bar-chart-o"></i> {{Historique}}</a></li>
                                 <?php
                                 if (Status::isConnectAdmin()) {
                                     ?>
-                                    <li><a href="index.php?v=d&p=report"><i class="fa fa-newspaper-o"></i>{{Rapport}}</a></li>
+                                    <li><a href="index.php?v=d&p=report"><i class="fa fa-newspaper-o"></i> {{Rapport}}</a></li>
                                     <?php
                                 }
                                 ?>
@@ -93,7 +93,7 @@ $panelMenu = PrepareView::getPanelMenu();
                                 if (Status::isConnectAdmin()) {
                                     ?>
                                     <li><a href="index.php?v=d&p=log"><i class="fa fa-file-o"></i> {{Logs}}</a></li>
-                                    <li><a href="index.php?v=d&p=eqAnalyse"><i class="fa fa-battery-full"></i>{{Equipements}}</a></li>
+                                    <li><a href="index.php?v=d&p=eqAnalyse"><i class="fa fa-battery-full"></i> {{Equipements}}</a></li>
                                     <li class="divider"></li>
                                     <li><a href="index.php?v=d&p=health"><i class="fa fa-medkit"></i> {{Santé}}</a></li>
                                     <?php
@@ -110,18 +110,17 @@ $panelMenu = PrepareView::getPanelMenu();
                                     class="caret"></b></a>
                                     <ul class="dropdown-menu" role="menu">
                                         <li><a href="index.php?v=d&p=object"><i class="fa fa-picture-o"></i> {{Objets}}</a></li>
-                                        <li><a href="index.php?v=d&p=interact"><i class="fa fa-comments-o"></i>{{Interactions}}</a></li>
+                                        <li><a href="index.php?v=d&p=interact"><i class="fa fa-comments-o"></i> {{Interactions}}</a></li>
                                         <li><a href="index.php?v=d&p=display"><i class="fa fa-th"></i> {{Résumé domotique}}</a></li>
                                         <li><a href="index.php?v=d&p=scenario"><i class="fa fa-cogs"></i> {{Scénarios}}</a></li>
                                     </ul>
                                 </li>
                                 <li class="dropdown cursor">
-                                    <a data-toggle="dropdown"><i class="fa fa-tasks fa-3x"></i><br> <span
-                                        class="hidden-xs hidden-sm hidden-md">{{Plugins}}</span> <b
+                                    <a data-toggle="dropdown"><i class="fa fa-puzzle-piece fa-3x"></i><br> <span
+                                        class="hidden-xs hidden-sm hidden-md"> {{Plugins}}</span> <b
                                         class="caret"></b></a>
                                         <ul class="dropdown-menu" role="menu">
-                                            <li><a href="index.php?v=d&p=plugin"><i class="fa fa-tags"></i> {{Gestion des
-                                                plugins}}</a></li>
+                                            <li><a href="index.php?v=d&p=plugin"><i class="fa fa-tags"></i> {{Gestion des plugins}}</a></li>
                                                 <li role="separator" class="divider"></li>
                                                 <?php
                                                 echo $pluginMenu;
@@ -173,24 +172,22 @@ $panelMenu = PrepareView::getPanelMenu();
                                                     <li><a href="index.php?v=d&p=migration"><i class="fa fa-floppy-o"></i> {{Migration depuis jeedom}}</a></li>
                                                     <li><a href="index.php?v=d&p=update"><i class="fa fa-refresh"></i> {{Centre de mise à jour}}</a></li>
                                                     <li><a href="index.php?v=d&p=cron"><i class="fa fa-tasks"></i> {{Moteur de tâches}}</a></li>
-                                                    <li><a href="index.php?v=d&p=custom"><i class="fa fa-pencil-square-o"></i>{{Personnalisation avancée}}</a></li>
+                                                    <li><a href="index.php?v=d&p=custom"><i class="fa fa-pencil-square-o"></i> {{Personnalisation avancée}}</a></li>
                                                     <li role="separator" class="divider"></li>
                                                     <li><a href="index.php?v=d&p=user"><i class="fa fa-users"></i> {{Utilisateurs}}</a></li>
-                                                    <li><a href="index.php?v=d&p=profils"><i class="fa fa-briefcase"></i>{{Profil}} <?php echo $_SESSION['user']->getLogin(); ?></a></li>
-                                                    <li><a href="index.php?v=m" class="noOnePageLoad"><i class="fa fa-mobile"></i> {{Versionmobile}}</a></li>
+                                                    <li><a href="index.php?v=d&p=profils"><i class="fa fa-briefcase"></i> {{Profil}} <?php echo $_SESSION['user']->getLogin(); ?></a></li>
+                                                    <li><a href="index.php?v=m" class="noOnePageLoad"><i class="fa fa-mobile"></i> {{Version mobile}}</a></li>
                                                     <li><a href="#" id="bt_nextdomAbout"><i class="fa fa-info-circle"></i> {{Version}}<?php echo nextdom::version(); ?></a></li>
                                                     <li><a href="index.php?v=d&logout=1" class="noOnePageLoad"><i class="fa fa-sign-out"></i> {{Se déconnecter}}</a></li>
-                                                    <li><a id="bt_getHelpPage" class="cursor" data-plugin="<?php echo init('m'); ?>"data-page="<?php echo init('p'); ?>" title="{{Aide sur la page en cours}}"><i class="fa fa-question-circle"></i></a></li>
+                                                    <li><a id="bt_getHelpPage" class="cursor" data-plugin="<?php echo init('m'); ?>"data-page="<?php echo init('p'); ?>" title="{{Aide sur la page en cours}}"><i class="fa fa-question-circle"></i> {{Aide}}</a></li>
                                                     <?php if (Status::isConnectAdmin()) {
                                                         ?>
                                                         <li>
                                                             <?php if (isset($plugin) && is_object($plugin) && $plugin->getIssue() != '') {
                                                                 ?>
-                                                                <a target="_blank" href="<?php echo $plugin->getIssue() ?>" title="{{Envoyer un rapport de bug}}"><i class="fa fa-exclamation-circle"></i></a>
+                                                                <a target="_blank" href="<?php echo $plugin->getIssue() ?>" title="{{Envoyer un rapport de bug}}"><i class="fa fa-exclamation-circle"></i> {{Rapport de bug}}</a>
                                                             <?php } else { ?>
-                                                                <a class="bt_reportBug cursor" title="{{Envoyer un rapport de bug}}">
-                                                                    <i class="fa fa-exclamation-circle"></i>
-                                                                </a>
+                                                                <a class="bt_reportBug cursor" title="{{Envoyer un rapport de bug}}"><i class="fa fa-exclamation-circle"></i> {{Rapport de bug}}</a>
                                                             <?php } ?>
                                                         </li>
                                                     <?php }
