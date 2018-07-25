8b500b92a04f29d317f92376ff162f0f9ced5c79
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-07-25 22:14:27 +0200

    Correction du regex

--- core/src/Helpers/Utils.php
+++ core/src/Helpers/Utils.php
@@ -192,7 +192,7 @@ class Utils
             '>' => '>',
             '' => ''
         ];
-        preg_match_all('/(\#.*?\#|\w+\(.*?\)|".*?"|\'.*?\'|\d+|\(|\))(?:[ ]*([!>=<adenortu]+)[ ]*)*/i', $expression, $preg_output);
+        preg_match_all('/(\w+|\d+|".*?"|\'.*?\'|\#.*?\#|\(|\))[ ]*([!>=<]+|and|or|ou|et)*[ ]*/i', $expression, $preg_output);
         if (count($preg_output) > 2) {
             $result = '';
             $exprIndex = 0;
