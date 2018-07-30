dc756ce30260e651697be661930b10d8ae425c6e
root <root@domo.lessissis.local>
2018-07-30 22:59:01 +0200

    Correction de l'ordre des mises à jours

--- todo.php
+++ todo.php
@@ -3,6 +3,7 @@
 require_once('core/config/common.config.php');
 global $CONFIG;
 
+shell_exec('mysql -u '.$CONFIG['db']['username'].' -p'.$CONFIG['db']['password'].' '.$CONFIG['db']['dbname'].' < install/update/nextdom.sql');
 shell_exec('php install/update/3.2.4.php');
 shell_exec('php install/update/3.2.5.php');
 shell_exec('php install/update/3.2.6.php');
@@ -11,5 +12,4 @@ shell_exec('mysql -u '.$CONFIG['db']['username'].' -p'.$CONFIG['db']['password']
 shell_exec('mysql -u '.$CONFIG['db']['username'].' -p'.$CONFIG['db']['password'].' '.$CONFIG['db']['dbname'].' < install/update/3.3.1.sql');
 shell_exec('mysql -u '.$CONFIG['db']['username'].' -p'.$CONFIG['db']['password'].' '.$CONFIG['db']['dbname'].' < install/update/3.3.2.sql');
 shell_exec('mysql -u '.$CONFIG['db']['username'].' -p'.$CONFIG['db']['password'].' '.$CONFIG['db']['dbname'].' < install/update/3.3.3.sql');
-shell_exec('mysql -u '.$CONFIG['db']['username'].' -p'.$CONFIG['db']['password'].' '.$CONFIG['db']['dbname'].' < install/update/nextdom.sql');
 
