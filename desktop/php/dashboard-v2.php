02b7dedfceb3575252129a89ee32313044956a66
Unknown <vincent.fresnel@gmail.com>
2018-07-30 17:34:19 +0200

    amélioration dashbaord-v2

--- desktop/php/dashboard-v2.php
+++ desktop/php/dashboard-v2.php
@@ -21,46 +21,7 @@ $child_object = jeeObject::buildTree($object);
 sendVarToJs('rootObjectId', $object->getId());
 ?>
 
-<div class="row row-overflow">
-    <?php
-    if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
-        echo '<div class="col-lg-2 col-md-3 col-sm-4" id="div_displayObjectList">';
-    } else {
-        echo '<div class="col-lg-2 col-md-3 col-sm-4" style="display:none;" id="div_displayObjectList">';
-    }
-    ?>
-    <div class="bs-sidebar">
-        <ul id="ul_object" class="nav nav-list bs-sidenav">
-            <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-            <?php
-            $allObject = jeeObject::buildTree(null, true);
-            foreach ($allObject as $object_li) {
-                $margin = 5 * $object_li->getConfiguration('parentNumber');
-                if ($object_li->getId() == $object->getId()) {
-                    echo '<li class="cursor li_object active" ><a data-object_id="' . $object_li->getId() . '" data-href="index.php?v=d&p=dashboard&object_id=' . $object_li->getId() . '&category=' . init('category', 'all') . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;font-size:0.85em;">' . $object_li->getHumanName(true, true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
-                } else {
-                    echo '<li class="cursor li_object" ><a data-object_id="' . $object_li->getId() . '" data-href="index.php?v=d&p=dashboard&object_id=' . $object_li->getId() . '&category=' . init('category', 'all') . '" style="padding: 2px 0px;"><span style="position:relative;left:' . $margin . 'px;font-size:0.85em;">' . $object_li->getHumanName(true, true) . '</span><span style="font-size : 0.65em;float:right;position:relative;top:7px;">' . $object_li->getHtmlSummary() . '</span></a></li>';
-                }
-            }
-            ?>
-        </ul>
-    </div>
-</div>
-<?php
-if ($_SESSION['user']->getOptions('displayScenarioByDefault') == 1) {
-    if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
-        echo '<div class="col-lg-8 col-md-7 col-sm-5" id="div_displayObject">';
-    } else {
-        echo '<div class="col-lg-10 col-md-9 col-sm-7" id="div_displayObject">';
-    }
-} else {
-    if ($_SESSION['user']->getOptions('displayObjetByDefault') == 1) {
-        echo '<div class="col-lg-10 col-md-9 col-sm-8" id="div_displayObject">';
-    } else {
-        echo '<div class="col-lg-12 col-md-12 col-sm-12" id="div_displayObject">';
-    }
-}
-?>
+
 <i class='fa fa-picture-o cursor pull-left' id='bt_displayObject' data-display='<?php echo $_SESSION['user']->getOptions('displayObjetByDefault') ?>' title="{{Afficher/Masquer les objets}}"></i>
 <i class='fa fa-sort-amount-desc pull-left cursor' id='bt_categorieHidden' title="{{Trier vos équipements}}"></i>
 <i class='fa fa-cogs pull-right cursor' id='bt_displayScenario' data-display='<?php echo $_SESSION['user']->getOptions('displayScenarioByDefault') ?>' title="{{Afficher/Masquer les scénarios}}"></i>
@@ -124,30 +85,30 @@ if ($_SESSION['user']->getOptions('displayScenarioByDefault') == 1) {
         echo '<div class="col-md-' . $object->getDisplay('dashboard::size', 12) . '">';
     }
 
-    echo '<div class="panel" style="border-color:' . $object->getDisplay("tagColor") . ';" data-father_id="' . $object->getFather_id() .'">';
-    echo '<div class="panel-heading" style="background-color:' . $object->getDisplay("tagColor") . '; color:'.$object->getDisplay("tagTextColor").'">';
-    echo '<h3 class="panel-title">' . $object->getDisplay("icon" ).' '. $object->getName().'</h3>';
-    echo '</div>';
-    echo '<div id="config">';
-    echo '<div class="panel-body">';
-    echo '<div class="div_displayEquipement" id="div_ob' . $object->getId() . '" style="width: 100%;padding-top:3px;margin-bottom : 3px;">';
+echo '<div class="panel" style="border-color:' . $object->getDisplay("tagColor") . ';" data-father_id="' . $object->getFather_id() .'">';
+echo '<div class="panel-heading" style="background-color:' . $object->getDisplay("tagColor") . '; color:'.$object->getDisplay("tagTextColor").'">';
+echo '<h3 class="panel-title">' . $object->getDisplay("icon" ).' '. $object->getName().'</h3>';
+echo '</div>';
+echo '<div id="config">';
+echo '<div class="panel-body">';
+echo '<div class="div_displayEquipement" id="div_ob' . $object->getId() . '" style="width: 100%;padding-top:3px;margin-bottom : 3px;">';
     echo '<script>getObjectHtml(' . $object->getId() . ')</script>';
     echo '</div>';
     echo '</div>';
     echo '</div>';
-    echo '</div>';
+echo '</div>';
     foreach ($child_object as $child) {
         if ($child->getConfiguration('hideOnDashboard', 0) == 1) {
             continue;
         }
-        echo '<div class="panel div_object" style="border-color:' . $child->getDisplay("tagColor") . ';" data-father_id="' . $child->getFather_id() .'">';
-        echo '<div class="panel-heading" style="background-color:' . $child->getDisplay("tagColor") . ';color:'.$child->getDisplay("tagTextColor").'">';
-        echo '<h3 class="panel-title">' . $child->getDisplay("icon" ).' '. $child->getName().'</h3>';
-        echo '      <span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-up"></i></span>';
-        echo '</div>';
-        echo '<div id="config">';
-        echo '<div class="panel-body">';
-        echo '<div class="div_displayEquipement" id="div_ob' . $child->getId() . '" style="width: 100%;padding-top:3px;margin-bottom : 3px;">';
+      echo '<div class="panel div_object" style="border-color:' . $child->getDisplay("tagColor") . ';" data-father_id="' . $child->getFather_id() .'">';
+echo '<div class="panel-heading" style="background-color:' . $child->getDisplay("tagColor") . ';color:'.$child->getDisplay("tagTextColor").'">';
+echo '<h3 class="panel-title">' . $child->getDisplay("icon" ).' '. $child->getName().'</h3>';
+echo '      <span class="pull-right clickable"><i class="glyphicon glyphicon-chevron-up"></i></span>';
+echo '</div>';
+echo '<div id="config">';
+echo '<div class="panel-body">';
+echo '<div class="div_displayEquipement" id="div_ob' . $child->getId() . '" style="width: 100%;padding-top:3px;margin-bottom : 3px;">';
         echo '<script>getObjectHtml(' . $child->getId() . ')</script>';
         echo '</div>';
         echo '</div>';
@@ -374,4 +335,4 @@ immediately after the control sidebar -->
 </div>
 <!-- ./wrapper -->
 </html>
-<?php include_file('desktop', 'dashboard-v2', 'js'); ?>
+                  <?php include_file('desktop', 'dashboard-v2', 'js'); ?>
