265d634b34cc45222a6bbc87c4614a07e24b74e7
ColonelMoutarde <4697568+ColonelMoutarde@users.noreply.github.com>
2018-08-05 12:43:44 +0200

    cleaning

--- core/src/app/app.php
+++ core/src/app/app.php
@@ -1,6 +1,6 @@
 <?php
 
-/* This file is part of NextDom.
+/* This file is part of NextDom Software.
  *
  * NextDom is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,7 +16,6 @@
  * along with NextDom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-
 $app = [];
 
 $app['db'] = function() {
@@ -24,13 +23,9 @@ $app['db'] = function() {
     return \NextDom\Singletons\ConnectDb::getConnection();
 };
 
-
-
-////////////////////////////////
-////        NextDom DAO    /////
-////////////////////////////////
-
-//TODO: appelez même si inutilisé ???
+///////////////////////////////
+////        NextDom DAO    ////
+///////////////////////////////
 
 /**
  * @param $app
