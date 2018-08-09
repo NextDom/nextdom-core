c0db146a8d39e2650635523621c4ad8d15ed436f
Unknown <vincent.fresnel@gmail.com>
2018-08-09 16:20:41 +0200

    Mise au standard graphique NextDom

--- desktop/php/log.php
+++ desktop/php/log.php
@@ -1,58 +1,68 @@
-<?php
-if (!isConnect('admin')) {
-    throw new Exception('{{401 - Accès non autorisé}}');
-}
+    <?php
+    if (!isConnect('admin')) {
+        throw new Exception('{{401 - Accès non autorisé}}');
+    }
 
-$page = init('page', 1);
-$logfile = init('logfile');
-$list_logfile = array();
-$dir = opendir('log/');
-$logExist = false;
-while ($file = readdir($dir)) {
-    if ($file != '.' && $file != '..' && $file != '.htaccess' && !is_dir('log/' . $file)) {
-        $list_logfile[] = $file;
-        if ($logfile == $file) {
-            $logExist = true;
+    $page = init('page', 1);
+    $logfile = init('logfile');
+    $list_logfile = array();
+    $dir = opendir('log/');
+    $logExist = false;
+    while ($file = readdir($dir)) {
+        if ($file != '.' && $file != '..' && $file != '.htaccess' && !is_dir('log/' . $file)) {
+            $list_logfile[] = $file;
+            if ($logfile == $file) {
+                $logExist = true;
+            }
         }
     }
-}
-natcasesort($list_logfile);
-?>
-<div class="row row-overflow">
-    <div class="col-lg-2 col-md-3 col-sm-4" id="div_displayLogList">
-        <div class="bs-sidebar">
-            <ul id="ul_object" class="nav nav-list bs-sidenav">
-                <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
-                <?php
-foreach ($list_logfile as $file) {
-    $flag = '<i class="fa fa-check" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:green;"></i>';
-    if (shell_exec('grep ERROR ' . __DIR__ . '/../../log/' . $file . ' | wc -l ') != 0) {
-        $flag = '<i class="fa fa-exclamation-triangle" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:red;"></i>';
-    } else if (shell_exec('grep WARNING ' . __DIR__ . '/../../log/' . $file . ' | wc -l ') != 0) {
-        $flag = '<i class="fa fa-exclamation-circle" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:orange;"></i>';
+    natcasesort($list_logfile);
+    ?>
+    <section class="content">
+        <div class="row row-overflow">
+            <div class="col-md-12 col-sm-12" id="div_resumePluginList" style="border-left: solid 1px #EEE; padding-left: 25px;">
+                <div class="box">
+
+                    <div class="box-body">
+        <div class="col-lg-2 col-md-3 col-sm-4" id="div_displayLogList">
+
+            <div class="bs-sidebar">
+                <ul id="ul_object" class="nav nav-list bs-sidenav">
+                    <li class="filter" style="margin-bottom: 5px;"><input class="filter form-control input-sm" placeholder="{{Rechercher}}" style="width: 100%"/></li>
+                    <?php
+    foreach ($list_logfile as $file) {
+        $flag = '<i class="fa fa-check" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:green;"></i>';
+        if (shell_exec('grep ERROR ' . __DIR__ . '/../../log/' . $file . ' | wc -l ') != 0) {
+            $flag = '<i class="fa fa-exclamation-triangle" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:red;"></i>';
+        } else if (shell_exec('grep WARNING ' . __DIR__ . '/../../log/' . $file . ' | wc -l ') != 0) {
+            $flag = '<i class="fa fa-exclamation-circle" style="font-weight: bold;float:left;display:inline;margin-top:8px;color:orange;"></i>';
+        }
+        if ($file == $logfile) {
+            echo '<li class="cursor li_log active" data-log="' . $file . '" >' . $flag . '<a>' . $file . ' (' . round(filesize('log/' . $file) / 1024) . ' Ko)</a></li>';
+        } else {
+            echo '<li class="cursor li_log" data-log="' . $file . '">' . $flag . '<a>' . $file . ' (' . round(filesize('log/' . $file) / 1024) . ' Ko)</a></li>';
+        }
     }
-    if ($file == $logfile) {
-        echo '<li class="cursor li_log active" data-log="' . $file . '" >' . $flag . '<a>' . $file . ' (' . round(filesize('log/' . $file) / 1024) . ' Ko)</a></li>';
-    } else {
-        echo '<li class="cursor li_log" data-log="' . $file . '">' . $flag . '<a>' . $file . ' (' . round(filesize('log/' . $file) / 1024) . ' Ko)</a></li>';
-    }
-}
-?>
-            </ul>
-        </div>
-    </div>
-    <div class="col-lg-10 col-md-9 col-sm-8">
-        <div class="row">
-            <div class="col-lg-12">
-                <a class="btn btn-danger pull-right" id="bt_removeAllLog"><i class="far fa-trash-alt"></i> {{Supprimer tous}}</a>
-                <a class="btn btn-danger pull-right" id="bt_removeLog"><i class="far fa-trash-alt"></i> {{Supprimer}}</a>
-                <a class="btn btn-warning pull-right" id="bt_clearLog"><i class="fas fa-times"></i> {{Vider}}</a>
-                <a class="btn btn-success pull-right" id="bt_downloadLog"><i class="fas fa-cloud-download-alt"></i> {{Télécharger}}</a>
-                <a class="btn btn-warning pull-right" data-state="1" id="bt_globalLogStopStart"><i class="fas fa-pause"></i> {{Pause}}</a>
-                <input style="max-width: 150px;" class="form-control pull-right" id="in_globalLogSearch" placeholder="{{Rechercher}}" />
+    ?>
+                </ul>
             </div>
         </div>
-        <pre id='pre_globallog' style='overflow: auto; height: calc(100% - 50px);width:100%;margin-top: 5px;'></pre>
+        <div class="col-lg-10 col-md-9 col-sm-8">
+            <div class="row">
+                <div class="col-lg-12">
+                    <a class="btn btn-danger pull-right" id="bt_removeAllLog"><i class="far fa-trash-alt"></i> {{Supprimer tous}}</a>
+                    <a class="btn btn-danger pull-right" id="bt_removeLog"><i class="far fa-trash-alt"></i> {{Supprimer}}</a>
+                    <a class="btn btn-warning pull-right" id="bt_clearLog"><i class="fas fa-times"></i> {{Vider}}</a>
+                    <a class="btn btn-success pull-right" id="bt_downloadLog"><i class="fas fa-cloud-download-alt"></i> {{Télécharger}}</a>
+                    <a class="btn btn-warning pull-right" data-state="1" id="bt_globalLogStopStart"><i class="fas fa-pause"></i> {{Pause}}</a>
+                    <input style="max-width: 150px;" class="form-control pull-right" id="in_globalLogSearch" placeholder="{{Rechercher}}" />
+                </div>
+            </div>
+            <pre id='pre_globallog' style='overflow: auto; height: calc(100% - 50px);width:100%;margin-top: 5px;'></pre>
+        </div>
     </div>
-</div>
-<?php include_file('desktop', 'log', 'js');?>
+            </div>
+        </div>
+        </div>
+                </section>
+    <?php include_file('desktop', 'log', 'js');?>
