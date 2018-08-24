165538f6ec9a1db4216ed99f7a2bafb6565ce8c6
Sylvaner <sylvain.dangin@gmail.com>
2018-08-24 17:38:09 +0200

    Test migration avec mise à jour

--- core/class/nextdom.class.php
+++ core/class/nextdom.class.php
@@ -683,6 +683,7 @@ class nextdom
             $cmd = NEXTDOM_ROOT . '/install/migrate_jeedom_to_nextdom.php "backup=' . $backupFilePath . '"';
             $cmd .= ' >> ' . \log::getPathToLog('restore') . ' 2>&1 &';
             \system::php($cmd, true);
+            \system::php(NEXTDOM_ROOT.'/todo.php');
         } else {
             global $BACKUP_FILE;
             $BACKUP_FILE = $backupFilePath;
