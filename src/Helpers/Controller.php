f7f8c3afb6dcc4fa18050d54ad0458160679f7be
Sylvaner <sylvain.dangin@gmail.com>
2018-08-24 08:30:35 +0200

    Passage des plans sous twig

--- src/Helpers/Controller.php
+++ src/Helpers/Controller.php
@@ -54,7 +54,9 @@ class Controller
         'view' => 'viewPage',
         'view_edit' => 'viewEditPage',
         'eqAnalyze' => 'eqAnalyzePage',
-        'eqAnalyse' => 'eqAnalyzePage'
+        'eqAnalyse' => 'eqAnalyzePage',
+        'aplan' => 'planPage',
+        'aplan3d' => 'plan3dPage'
     ];
 
     /**
@@ -1188,5 +1190,117 @@ class Controller
         $pageContent['JS_END_POOL'][] = '/desktop/js/eqAnalyse.js';
 
         return $render->get('/desktop/eqAnalyze.html.twig', $pageContent);
+    }
+
+    /**
+     * Render plan page
+     *
+     * @param Render $render Render engine
+     * @param array $pageContent Page data
+     *
+     * @return string Content of plan page
+     *
+     * @throws \NextDom\Exceptions\CoreException
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public static function planPage(Render $render, array &$pageContent): string
+    {
+        Status::initConnectState();
+        Status::isConnectedOrFail();
+
+
+        $planHeader = null;
+        $planHeaders = \planHeader::all();
+        $planHeadersSendToJS = array();
+        foreach ($planHeaders as $planHeader_select) {
+            $planHeadersSendToJS[] = array('id' => $planHeader_select->getId(), 'name' => $planHeader_select->getName());
+        }
+        $pageContent['JS_VARS_RAW']['planHeader'] = Utils::getArrayToJQueryJson($planHeadersSendToJS);
+        if (Utils::init('plan_id') == '') {
+            foreach ($planHeaders as $planHeader_select) {
+                if ($planHeader_select->getId() == $_SESSION['user']->getOptions('defaultDashboardPlan')) {
+                    $planHeader = $planHeader_select;
+                    break;
+                }
+            }
+        } else {
+            foreach ($planHeaders as $planHeader_select) {
+                if ($planHeader_select->getId() == Utils::init('plan_id')) {
+                    $planHeader = $planHeader_select;
+                    break;
+                }
+            }
+        }
+        if (!is_object($planHeader) && count($planHeaders) > 0) {
+            $planHeader = $planHeaders[0];
+        }
+        if (!is_object($planHeader)) {
+            $pageContent['planHeaderError'] = true;
+            $pageContent['JS_VARS']['planHeader_id'] = -1;
+        } else {
+            $pageContent['planHeaderError'] = false;
+            $pageContent['JS_VARS']['planHeader_id'] = $planHeader->getId();
+        }
+
+        $pageContent['JS_END_POOL'][] = '/desktop/js/plan.js';
+
+        return $render->get('/desktop/plan.html.twig', $pageContent);
+    }
+
+    /**
+     * Render 3d plan page
+     *
+     * @param Render $render Render engine
+     * @param array $pageContent Page data
+     *
+     * @return string Content of 3d plan page
+     *
+     * @throws \NextDom\Exceptions\CoreException
+     * @throws \Twig_Error_Loader
+     * @throws \Twig_Error_Runtime
+     * @throws \Twig_Error_Syntax
+     */
+    public static function plan3dPage(Render $render, array &$pageContent): string
+    {
+        Status::initConnectState();
+        Status::isConnectedOrFail();
+
+        $plan3dHeader = null;
+        $list_plan3dHeader = \plan3dHeader::all();
+        if (Utils::init('plan3d_id') == '') {
+            if ($_SESSION['user']->getOptions('defaultDesktopPlan3d') != '') {
+                $plan3dHeader = \plan3dHeader::byId($_SESSION['user']->getOptions('defaultDesktopPlan3d'));
+            }
+            if (!is_object($plan3dHeader)) {
+                $plan3dHeader = $list_plan3dHeader[0];
+            }
+        } else {
+            $plan3dHeader = \plan3dHeader::byId(Utils::init('plan3d_id'));
+            if (!is_object($plan3dHeader)) {
+                $plan3dHeader = $list_plan3dHeader[0];
+            }
+        }
+        if (is_object($plan3dHeader)) {
+            $pageContent['JS_VARS']['plan3dHeader_id'] = $plan3dHeader->getId();
+            $pageContent['plan3dCurrentHeaderId'] = $plan3dHeader->getId();
+        } else {
+            $pageContent['JS_VARS']['plan3dHeader_id'] = -1;
+        }
+        $pageContent['plan3dHeader'] = \plan3dHeader::all();
+        $pageContent['plan3dFullScreen'] = Utils::init('fullscreen') == 1;
+
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/three.min.js';
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/loaders/LoaderSupport.js';
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/loaders/OBJLoader.js';
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/loaders/MTLLoader.js';
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/controls/TrackballControls.js';
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/controls/OrbitControls.js';
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/renderers/Projector.js';
+        $pageContent['JS_END_POOL'][] = '/3rdparty/three.js/objects/Sky.js';
+        $pageContent['JS_END_POOL'][] = '/desktop/js/plan3d.js';
+
+        return $render->get('/desktop/plan3d.html.twig', $pageContent);
     }
 }
