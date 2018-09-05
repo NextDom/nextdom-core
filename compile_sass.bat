12b10df17aa9c7e0598a7893185466ac63b555db
cyrilphoenix <cyrilphoenix71@gmail.com>
2018-09-05 13:34:57 +0200

    Compile

--- compile_sass.bat
+++ compile_sass.bat
@@ -1,1 +1,1 @@
-sass sass/dashboard.scss css/dashboard.css && sass sass/nextdom.scss css/nextdom.css && sass sass/nextdom.mob.scss css/nextdom.mob.css && sass sass/dashboard.scss css/dashboard.css
+sass sass/nextdom.scss css/nextdom.css && sass sass/nextdom.mob.scss css/nextdom.mob.css
