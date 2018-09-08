f3a5aa4b43941d2f63abdb56b51469a2b0e4f282
edgd1er <edgd1er@hotmail.com>
2018-09-08 16:35:44 +0200

    Docker: installation de deux conteneurs: 1 apache, 1 mysql (commit 2/2)
    - simplification du init.sh
    - besoin de lancer manuellement une fois, le init.sh (contrainte depot privé)

--- install/OS_specific/Docker/init.sh
+++ install/OS_specific/Docker/init.sh
@@ -6,28 +6,20 @@ if ! [ -f /.dockerinit ]; then
 	chmod 755 /.dockerinit
 fi
 
-if [ -z ${ROOT_PASSWORD} ]; then
-	ROOT_PASSWORD=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 20)
-	echo "Use generate password : ${ROOT_PASSWORD}"
-fi
-
+ROOT_PASSWORD=${ROOT_PASSWORD:--$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 20)}
+echo "Use generate password : ${ROOT_PASSWORD}"
 echo "root:${ROOT_PASSWORD}" | chpasswd
 
-if [ ! -z ${APACHE_PORT} ]; then
-	echo 'Change apache listen port to : '${APACHE_PORT}
-	echo "Listen ${APACHE_PORT}" > /etc/apache2/ports.conf
-	sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:${APACHE_PORT}/" /etc/apache2/sites-enabled/000-default.conf
-else
-	echo "Listen 80" > /etc/apache2/ports.conf
-	sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:80/" /etc/apache2/sites-enabled/000-default.conf
-fi	
+APACHE_PORT=${APACHE_PORT:-80}
+echo 'apache listen port: '${APACHE_PORT}
+echo "Listen ${APACHE_PORT}" > /etc/apache2/ports.conf
+sed -i -E "s/\<VirtualHost \*:(.*)\>/VirtualHost \*:${APACHE_PORT}/" /etc/apache2/sites-enabled/000-default.conf
 
-if [ ! -z ${SSH_PORT} ]; then
-	echo 'Change SSH listen port to : '${SSH_PORT}
-	echo "Port ${SSH_PORT}" >> /etc/ssh/sshd_config
-else
-	sed '/Port /d' /etc/ssh/sshd_config | echo "Port 22" >> /etc/ssh/sshd_config
-fi
+SSH_PORT=${SSH_PORT:-22}
+echo 'Change SSH listen port to : '${SSH_PORT}
+sed '/Port /d' /etc/ssh/sshd_config | echo "Port ${SSH_PORT}" >> /etc/ssh/sshd_config
+#Pdt les devs
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
 
 if [ ! -z ${MODE_HOST} ] && [ ${MODE_HOST} -eq 1 ]; then
 	echo 'Update /etc/hosts for host mode'
@@ -38,13 +30,16 @@ if [ -f "/var/www/html/_nextdom_is_installed" ]; then
 	echo 'NextDom is already install'
 else
 	echo 'Start nextdom installation'
-	#TODO tant que ce n'est pas dans le master commenter les 4 lines suivantes
+	#TODO tant que le dépot n'est pas publique commenter les 2 lines suivantes
+	#TODO il est déposé lors du build de l'image.
 	#rm -rf /root/install.sh
 	#wget https://raw.githubusercontent.com/nextdom/core/stable/install/install.sh -O /root/install.sh
-	#cp /var/www/html/core/install/install.sh /root/install.sh
-	#chmod +x /root/install.sh
+	[[ $(stat -c%s /root/install.sh) -lt 2 ]] && echo "Erreur, install.sh est incorrect" && exit -1
+	chmod +x /root/install.sh
 	#on reprend l'install la ou l'image s'est arrétée
-	/root/install_docker.sh -s 14 -v ${VERSION} -m ${SHELL_ROOT_PASSWORD} -n ${SHELL_ROOT_PASSWORD} -d ${MYSQL_HOST} -o
+	/root/install.sh -s 10 -v ${VERSION} -m ${SHELL_ROOT_PASSWORD} -n ${SHELL_ROOT_PASSWORD} -d ${MYSQL_HOST} -o
+	[[ $? -ne 0 ]] && echo "Erreur, install.sh s'est terminé en erreur" && exit -1
+	touch /var/www/html/_nextdom_is_installed
 fi
 
 echo 'All init complete'
@@ -55,10 +50,10 @@ chown -R www-data:www-data /var/www/html
 
 echo 'Start apache2'
 systemctl restart apache2
-service apache2 restart 
+service apache2 restart
 
 echo 'Start sshd'
 systemctl restart sshd
 service ssh restart
 
-/usr/bin/supervisord
+[[ $(ps -C cron | wc -l) -lt 2 ]] && /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
