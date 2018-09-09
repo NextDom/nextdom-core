0835756df33693d017a042271e873eebfc7ad2ba
Sylvaner <sylvain.dangin@gmail.com>
2018-09-09 15:26:52 +0200

    Retouches wizard

--- assets/js/desktop/firstUse.js
+++ assets/js/desktop/firstUse.js
@@ -1,91 +1,96 @@
 $(document).ready(function () {
-    var navListItems = $('div.setup-panel div a'),
-        allWells = $('.setup-content'),
-        allNextBtn = $('.nextBtn'),
-        allReturnBtn = $('.returnBtn');
+    var navListItems = $('div.setup-panel div a');
+    var allWells = $('.setup-content');
+    var allNextBtn = $('.nextBtn');
+//    var allReturnBtn = $('.returnBtn');
 
     allWells.hide();
 
     navListItems.click(function (e) {
         e.preventDefault();
-        var $target = $($(this).attr('href')),
-            $item = $(this);
+        var target = $($(this).attr('href'));
+        var item = $(this);
 
-        if (!$item.hasClass('disabled')) {
+        if (!item.hasClass('disabled')) {
             navListItems.removeClass('btn-primary').addClass('btn-default');
-            $item.addClass('btn-primary');
+            item.addClass('btn-primary');
             allWells.hide();
-            $target.show();
-            $target.find('input:eq(0)').focus();
+            target.show();
+            target.find('input:eq(0)').focus();
         }
     });
 
-    allNextBtn.click(function(){
-        var curStep = $(this).closest(".setup-content"),
-            curStepBtn = curStep.attr("id"),
-            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
-            isValid = true;
+    allNextBtn.click(function () {
+        var curStep = $(this).closest(".setup-content");
+        var curStepBtn = curStep.attr("id");
+        var nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a");
+        var curInputs = curStep.find("input[type='text'],input[type='url']");
+        isValid = true;
 
         $(".form-group").removeClass("has-error");
-        for(var i=0; i<curInputs.length; i++){
-            if (!curInputs[i].validity.valid){
+        for (var i = 0; i < curInputs.length; i++) {
+            if (!curInputs[i].validity.valid) {
                 isValid = false;
                 $(curInputs[i]).closest(".form-group").addClass("has-error");
             }
         }
 
-        if (isValid)
+        if (isValid) {
             nextStepWizard.removeAttr('disabled').trigger('click');
+        }
     });
 
-    allReturnBtn.click(function(){
-        var curStep = $(this).closest(".setup-content"),
-            curStepBtn = curStep.attr("id"),
-            returnStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a"),
-            curInputs = curStep.find("input[type='text'],input[type='url']"),
-            isValid = true;
+    /*
+    allReturnBtn.click(function () {
+        var curStep = $(this).closest(".setup-content");
+        var curStepBtn = curStep.attr("id");
+        var returnStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
+        var curInputs = curStep.find("input[type='text'],input[type='url']");
+        isValid = true;
 
         $(".form-group").removeClass("has-error");
-        for(var i=0; i<curInputs.length; i++){
-            if (!curInputs[i].validity.valid){
+        for (var i = 0; i < curInputs.length; i++) {
+            if (!curInputs[i].validity.valid) {
                 isValid = false;
                 $(curInputs[i]).closest(".form-group").addClass("has-error");
             }
         }
     });
+    */
+
     $('div.setup-panel div a.btn-primary').trigger('click');
 });
-$("#toStep2").click(function(){
+
+$("#toStep2").click(function () {
     jeedom.user.login({
         username: "admin",
         password: "admin",
         error: function (error) {
-            notify("Core",error.message,"error");
+            notify("Core", error.message, "error");
         },
         success: function (data) {
         }
     });
 });
-$("#toStep3").click(function(){
-    if($('#in_change_password').val() == $('#in_change_passwordToo').val()){
+$("#toStep3").click(function () {
+    if ($('#in_change_password').val() == $('#in_change_passwordToo').val()) {
         jeedom.user.get({
             error: function (data) {
-                notify("Erreur",data.message,"error");
+                notify("Erreur", data.message, "error");
             },
-            success: function (data){
+            success: function (data) {
                 var user = data;
                 user.password = $('#in_change_password').val();
                 jeedom.user.saveProfils({
                     profils: user,
                     error: function (error) {
-                        notify("Erreur",error.message,"error");
+                        notify("Erreur", error.message, "error");
                     },
-                    success : function (){
+                    success: function () {
                         jeedom.config.load({
                             configuration: 'market::username',
                             error: function (error) {
-                                notify("Erreur",error.message,"error");
+                                notify("Erreur", error.message, "error");
 
                             },
                             success: function (data) {
@@ -95,31 +100,31 @@ $("#toStep3").click(function(){
                 });
             }
         });
-    }else{
-        notify("Erreur","Les deux mots de passe ne sont pas identiques","error")
+    } else {
+        notify("Erreur", "Les deux mots de passe ne sont pas identiques", "error")
     }
 });
 
-$("#toStep4").click(function(){
+$("#toStep4").click(function () {
     var username = $('#in_login_username_market').val();
     var password = $('#in_login_password_market').val();
-    var adress = 'https://jeedom.com/market';
+    var address = 'https://jeedom.com/market';
     jeedom.config.save({
         configuration: {'market::username': username},
         error: function (error) {
-            notify("Erreur",data.message,"error");
+            notify("Erreur", data.message, "error");
         },
         success: function (data) {
             jeedom.config.save({
                 configuration: {'market::password': password},
                 error: function (error) {
-                    notify("Erreur",error.message,"error");
+                    notify("Erreur", error.message, "error");
                 },
                 success: function (data) {
                     jeedom.repo.test({
                         repo: 'market',
                         error: function (error) {
-                            notify("Erreur",error.message,"error");
+                            notify("Erreur", error.message, "error");
                         },
                         success: function (data) {
                         }
@@ -130,19 +135,19 @@ $("#toStep4").click(function(){
     });
 });
 
-    $("#toStep5").click(function(){
+$("#toStep5").click(function () {
 
-    });
+});
 
-    $("#finishConf").click(function(){
-        nextdom.config.save({
-            configuration: {'nextdom::firstUse': 0},
-            error: function (error) {
-                notify("Core", error.message, 'error');
-            },
-            success: function () {
-                notify("Core", '{{Sauvegarde réussie}}', 'success');
-                location.reload();
-            }
-        });
+$("#finishConf").click(function () {
+    nextdom.config.save({
+        configuration: {'nextdom::firstUse': 0},
+        error: function (error) {
+            notify("Core", error.message, 'error');
+        },
+        success: function () {
+            notify("Core", '{{Sauvegarde réussie}}', 'success');
+            location.reload();
+        }
     });
+});
