a85b4767800ebcd1f936eb976dca99a152fd9149
Unknown <vincent.fresnel@gmail.com>
2018-09-10 11:48:18 +0200

    amélioration firstuse

--- assets/js/desktop/firstUse.js
+++ assets/js/desktop/firstUse.js
@@ -1,27 +1,115 @@
-$(document).ready(function () {
-    var navListItems = $('div.setup-panel div a');
-    var allWells = $('.setup-content');
-    var allNextBtn = $('.nextBtn');
-//    var allReturnBtn = $('.returnBtn');
+    $(document).ready(function () {
+        var navListItems = $('div.setup-panel div a');
+        var allWells = $('.setup-content');
+        var allNextBtn = $('.nextBtn');
+    //    var allReturnBtn = $('.returnBtn');
 
-    allWells.hide();
+        allWells.hide();
 
-    navListItems.click(function (e) {
-        e.preventDefault();
-        var target = $($(this).attr('href'));
-        var item = $(this);
+        navListItems.click(function (e) {
+            e.preventDefault();
+            var target = $($(this).attr('href'));
+            var item = $(this);
 
-        if (!item.hasClass('disabled')) {
-            navListItems.removeClass('btn-primary').addClass('btn-default');
-            item.addClass('btn-primary');
-            allWells.hide();
-            target.show();
-            target.find('input:eq(0)').focus();
+            if (!item.hasClass('disabled')) {
+                navListItems.removeClass('btn-primary').addClass('btn-default');
+                item.addClass('btn-primary');
+                allWells.hide();
+                target.show();
+                target.find('input:eq(0)').focus();
+            }
+        });
+
+
+        $('div.setup-panel div a.btn-primary').trigger('click');
+    });
+
+    $("#toStep2").click(function () {
+        jeedom.user.login({
+            username: "admin",
+            password: "admin",
+            error: function (error) {
+                notify("Core", error.message, "error");
+            },
+            success: function (data) {
+                NextStep("#toStep2");
+            }
+        });
+    });
+    $("#toStep3").click(function () {
+        if ($('#in_change_password').val() == $('#in_change_passwordToo').val()) {
+            jeedom.user.get({
+                error: function (data) {
+                    notify("Core", data.message, "error");
+                },
+                success: function (data) {
+                    var user = data;
+                    user.password = $('#in_change_password').val();
+                    jeedom.user.saveProfils({
+                        profils: user,
+                        error: function (error) {
+                            notify("Core", error.message, "error");
+                        },
+                        success: function () {
+                        }
+                    });
+                }
+            });
+            nextdom.config.save({
+                configuration: {'nextdom::firstUse': 0},
+                error: function (error) {
+                    notify("Core", error.message, 'error');
+                },
+                success: function () {
+                    notify("Core", '{{Mot de passe changé avec success}}', 'success');
+                    NextStep("#toStep3");
+                }
+            });
+        } else {
+            notify("Erreur", "Les deux mots de passe ne sont pas identiques", "error")
         }
     });
 
-    allNextBtn.click(function () {
-        var curStep = $(this).closest(".setup-content");
+    $("#toStep4").click(function () {
+        var username = $('#in_login_username_market').val();
+        var password = $('#in_login_password_market').val();
+        var address = 'https://jeedom.com/market';
+        jeedom.config.save({
+            configuration: {'market::username': username},
+            error: function (error) {
+                notify("Core", data.message, "error");
+            },
+            success: function (data) {
+                jeedom.config.save({
+                    configuration: {'market::password': password},
+                    error: function (error) {
+                        notify("Core", error.message, "error");
+                    },
+                    success: function (data) {
+                        jeedom.repo.test({
+                            repo: 'market',
+                            error: function (error) {
+                                notify("Core", error.message, "error");
+                            },
+                            success: function (data) {
+                                NextStep("#toStep4");
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+    $("#skipStep4").click(function () {
+        NextStep("#toStep4");
+    }
+
+    $("#finishConf").click(function () {
+        Location.reload();
+    });
+
+    function NextStep(_step) {
+        var curStep = $(_step).closest(".setup-content");
         var curStepBtn = curStep.attr("id");
         var nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a");
         var curInputs = curStep.find("input[type='text'],input[type='url']");
@@ -38,116 +126,4 @@ $(document).ready(function () {
         if (isValid) {
             nextStepWizard.removeAttr('disabled').trigger('click');
         }
-    });
-
-    /*
-    allReturnBtn.click(function () {
-        var curStep = $(this).closest(".setup-content");
-        var curStepBtn = curStep.attr("id");
-        var returnStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().prev().children("a");
-        var curInputs = curStep.find("input[type='text'],input[type='url']");
-        isValid = true;
-
-        $(".form-group").removeClass("has-error");
-        for (var i = 0; i < curInputs.length; i++) {
-            if (!curInputs[i].validity.valid) {
-                isValid = false;
-                $(curInputs[i]).closest(".form-group").addClass("has-error");
-            }
-        }
-    });
-    */
-
-    $('div.setup-panel div a.btn-primary').trigger('click');
-});
-
-$("#toStep2").click(function () {
-    jeedom.user.login({
-        username: "admin",
-        password: "admin",
-        error: function (error) {
-            notify("Core", error.message, "error");
-        },
-        success: function (data) {
-        }
-    });
-});
-$("#toStep3").click(function () {
-    if ($('#in_change_password').val() == $('#in_change_passwordToo').val()) {
-        jeedom.user.get({
-            error: function (data) {
-                notify("Erreur", data.message, "error");
-            },
-            success: function (data) {
-                var user = data;
-                user.password = $('#in_change_password').val();
-                jeedom.user.saveProfils({
-                    profils: user,
-                    error: function (error) {
-                        notify("Erreur", error.message, "error");
-                    },
-                    success: function () {
-                        jeedom.config.load({
-                            configuration: 'market::username',
-                            error: function (error) {
-                                notify("Erreur", error.message, "error");
-
-                            },
-                            success: function (data) {
-                            }
-                        });
-                    }
-                });
-            }
-        });
-    } else {
-        notify("Erreur", "Les deux mots de passe ne sont pas identiques", "error")
     }
-});
-
-$("#toStep4").click(function () {
-    var username = $('#in_login_username_market').val();
-    var password = $('#in_login_password_market').val();
-    var address = 'https://jeedom.com/market';
-    jeedom.config.save({
-        configuration: {'market::username': username},
-        error: function (error) {
-            notify("Erreur", data.message, "error");
-        },
-        success: function (data) {
-            jeedom.config.save({
-                configuration: {'market::password': password},
-                error: function (error) {
-                    notify("Erreur", error.message, "error");
-                },
-                success: function (data) {
-                    jeedom.repo.test({
-                        repo: 'market',
-                        error: function (error) {
-                            notify("Erreur", error.message, "error");
-                        },
-                        success: function (data) {
-                        }
-                    });
-                }
-            });
-        }
-    });
-});
-
-$("#toStep5").click(function () {
-
-});
-
-$("#finishConf").click(function () {
-    nextdom.config.save({
-        configuration: {'nextdom::firstUse': 0},
-        error: function (error) {
-            notify("Core", error.message, 'error');
-        },
-        success: function () {
-            notify("Core", '{{Sauvegarde réussie}}', 'success');
-            location.reload();
-        }
-    });
-});
