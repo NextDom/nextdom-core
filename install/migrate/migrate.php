a227d884b9d8b74b4ea0d75b9721c4efb1cdc8ed
Sylvaner <sylvain.dangin@gmail.com>
2018-09-16 12:05:46 +0200

    Nettoyage des scripts

--- install/migrate/migrate.php
+++ install/migrate/migrate.php
@@ -1,5 +1,15 @@
 <?php
+
 require_once __DIR__ . '/../../core/php/core.inc.php';
+require_once(__DIR__.'/../../core/config/common.config.php');
+
+global $CONFIG;
+
+foreach (interactDef::all() as $interactDef) {
+    $interactDef->setEnable(1);
+    $interactDef->save();
+}
+
 foreach (scenarioExpression::all() as $scenarioExpression) {
     if ($scenarioExpression->getExpression() == 'equipment') {
         try {
@@ -10,3 +20,6 @@ foreach (scenarioExpression::all() as $scenarioExpression) {
         }
     }
 }
+
+shell_exec('mysql -u '.$CONFIG['db']['username'].' -p'.$CONFIG['db']['password'].' -f '.$CONFIG['db']['dbname'].' < '.__DIR__.'/migrate.sql');
+
