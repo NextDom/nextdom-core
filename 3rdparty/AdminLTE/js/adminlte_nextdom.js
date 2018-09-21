1be0c73c5240eecb3e36ab83c2a4d1ac2920a5b6
Unknown <vincent.fresnel@gmail.com>
2018-09-21 14:20:45 +0200

    retour arriere

--- 3rdparty/AdminLTE/js/adminlte_nextdom.js
+++ 3rdparty/AdminLTE/js/adminlte_nextdom.js
@@ -5,3 +5,11 @@ $(function() {
 $(function() {
 $(":input").inputmask();
 });
+
+$(function () {
+    $('input').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' /* optional */
+    });
+});
