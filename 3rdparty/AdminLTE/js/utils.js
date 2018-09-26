c4738328308e223217cfcc2e3483aee8d35b36e1
Unknown <vincent.fresnel@gmail.com>
2018-09-26 14:54:50 +0200

    readd icheck

--- 3rdparty/AdminLTE/js/utils.js
+++ 3rdparty/AdminLTE/js/utils.js
@@ -4,9 +4,25 @@ $(function() {
 $(":input").inputmask();
 
 $('[data-toggle="tooltip"]').tooltip();
+
+$(document).ready(function(){
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_minimal-blue',
+      radioClass: 'iradio_minimal-blue',
+      increaseArea: '20%' // optional
+    });
+  });
 });
 
 if ($.fn.button.noConflict) {
     var bootstrapButton = $.fn.button.noConflict();
     $.fn.bootstrapBtn = bootstrapButton;
-    }
+}
+
+$(document).on('icheck', function(){
+    $('input[type=checkbox], input[type=radio]').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue',
+        increaseArea: '20%' // optional
+    });
+    }).trigger('icheck'); // trigger it for page load
