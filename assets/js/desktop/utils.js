2143e290269e841b71db5b5861c91e1eef4011d1
byackee <vincent.fresnel@gmail.com>
2018-10-02 10:39:49 +0200

    login page with skin + add fullscreen

--- assets/js/desktop/utils.js
+++ assets/js/desktop/utils.js
@@ -998,3 +998,28 @@ function hexToRgb(hex) {
         b: parseInt(result[3], 16)
     } : null;
 }
+
+function toggleFullScreen() {
+    if ((document.fullScreenElement && document.fullScreenElement !== null) ||    // alternative standard method
+        (!document.mozFullScreen && !document.webkitIsFullScreen)) {               // current working methods
+        if (document.documentElement.requestFullScreen) {
+            document.documentElement.requestFullScreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullScreen) {
+            document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+        $('#togglefullscreen').removeClass('fa-expand').addClass('fa-compress');
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+        $('#togglefullscreen').removeClass('fa-compress').addClass('fa-expand');
+
+    }
+
+}
