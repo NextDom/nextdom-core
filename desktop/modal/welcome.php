c34a76529e60cb519557f7af34e1b2bb7ab2049d
cyrilphoenix <cyrilphoenix71@gmail.com>
2018-10-07 23:18:19 +0200

    fix welcome

--- desktop/modal/welcome.php
+++ desktop/modal/welcome.php
@@ -4,10 +4,10 @@ if (!isConnect()) {
 }
 ?>
 
-<span class='btn btn-legend btn-action btn-legend pull-right' id='bt_doNotDisplayWelcome'><i class="fas fa-eye-slash">&nbsp;&nbsp;</i>{{ 'Ne plus afficher' }}</span>
 <div class="alert alert-success">
-    <h4><i class="fas fa-thumbs-up"></i>{{ 'Félicitations' }}</h4>
-    {{ 'Bienvenue dans' }} <?= config::byKey('product_name'); ?> {{ ', merci d\'avoir choisit cet outil pour votre habitat connecté.' '}}<br>
+    <h4><i class="fas fa-thumbs-up"></i>{{Félicitations}}</h4>
+    {{Bienvenue dans}} <?= config::byKey('product_name'); ?> {{, merci d\'avoir choisit cet outil pour votre habitat connecté.'}}
+    <span class='btn btn-legend btn-action btn-legend pull-right' id='bt_doNotDisplayWelcome'><i class="fas fa-eye-slash">&nbsp;&nbsp;</i>{{Ne plus afficher}}</span>
 </div>
 
 <div class="container-fluid">
@@ -15,7 +15,7 @@ if (!isConnect()) {
 		<div class="col-md-6">
 			<div class="box box-solid">
 				<div class="box-header with-border">
-          <i class="fas fa-puzzle-piece"></i><h3 class="box-title">{{ 'Plugins populaires' }}</h3>
+          <i class="fas fa-puzzle-piece"></i><h3 class="box-title">{{Plugins populaires}}</h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
@@ -63,15 +63,15 @@ if (!isConnect()) {
 		<div class="col-md-6">
 			<div class="box box-solid">
 				<div class="box-header with-border">
-					<h3 class="box-title">{{ 'Migration Jeedom' }}&nbsp;&nbsp;</h3>
+					<h3 class="box-title">{{Migration Jeedom}}&nbsp;&nbsp;</h3>
           <i class="fas fa-caret-right">&nbsp;</i>
-          <h3 class="box-title">{{ 'NextDom' }}</h3>
+          <h3 class="box-title">{{NextDom}}</h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
 					<div class="row text-center">
-						<div class="col-xs-12">
-							<a href="index.php?v=d&p=migration"><i class="fas fa-upload" style="font-size:14.2em;color:#33B8CC"></i></a>
+						<div class="form-group col-xs-12">
+							<a href="index.php?v=d&p=migration"><i class="fas fa-upload" style="font-size:14.3em;color:#33B8CC"></i></a>
 						</div>
 					</div>
 				</div>
@@ -86,13 +86,13 @@ if (!isConnect()) {
 			<div class="box box-solid">
 				<div class="box-header with-border">
 					<i class="fas fa-book"></i>
-					<h3 class="box-title">{{ 'Documentations' }}</h3>
+					<h3 class="box-title">{{Documentations}}</h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
-					<p class="lead">{{ 'Informez-vous pour mieux maitriser' }}</p>
-					<p class="text-muted">{{ 'La lecture de la documentation est fastidieuse mais c'est une opération essentielle pour bien maitriser votre solution domotique.' }}</p>
-			    <p><a href="http://www.nextdom.org/docs" class="text-blue">{{ 'Accéder à la documentation' }}</a></p>
+					<p class="lead">{{Informez-vous pour mieux maitriser}}</p>
+					<p class="text-muted">{{La lecture de la documentation est fastidieuse mais c'est une opération essentielle pour bien maitriser votre solution domotique.}}</p>
+			    <p><a href="http://www.nextdom.org/docs" class="text-blue">{{Accéder à la documentation}}</a></p>
 				</div>
 				<!-- /.box-body -->
 			</div>
@@ -101,16 +101,16 @@ if (!isConnect()) {
 			<div class="box box-solid">
 				<div class="box-header with-border">
 					<i class="fas fa-comments"></i>
-					<h3 class="box-title">{{ 'Forum' }}</h3>
+					<h3 class="box-title">{{Forum}}</h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
-					<p class="lead">{{ 'Trouver de l'aide sur notre forum' }}</p>
+					<p class="lead">{{Trouver de l'aide sur notre forum}}</p>
 					<p class="text-muted">
-					  {{ 'Le forum NextDom est un moyen simple et efficace de trouver la solution a votre probleme.<br/>
+					  {{Le forum NextDom est un moyen simple et efficace de trouver la solution a votre probleme.<br/>
 						Il y a surement une autre personne qui a déja rencontré le meme probleme que vous.<br/>
-						Si ce n'est pas le cas, posez vous question, la communautée y répondra au plus vite.' }}</p>
-					<p><a href="http://www.nextdom.org/forum" class="text-blue">{{ 'Accéder au forum' }}</a><p/>
+						Si ce n'est pas le cas, posez vous question, la communautée y répondra au plus vite.}}</p>
+					<p><a href="http://www.nextdom.org/forum" class="text-blue">{{Accéder au forum}}</a><p/>
 				</div>
 				<!-- /.box-body -->
 			</div>
@@ -119,13 +119,13 @@ if (!isConnect()) {
 			<div class="box box-solid">
 				<div class="box-header with-border">
 					<i class="fas fa-rss-square"></i>
-					<h3 class="box-title">{{ 'Blog' }}</h3>
+					<h3 class="box-title">{{Blog}}</h3>
 				</div>
 				<!-- /.box-header -->
 				<div class="box-body">
-					<p class="lead">{{ 'Restez informé' }}</p>
-					<p class="text-muted">{{ 'Retrouvez toute l'actualité de NextDom sur notre blog.' }}</p>
-					<p><a href="http://www.nextdom.org/blog" class="text-blue">{{ 'Accéder au blog' }}</a></p>
+					<p class="lead">{{Restez informé}}</p>
+					<p class="text-muted">{{Retrouvez toute l'actualité de NextDom sur notre blog.}}</p>
+					<p><a href="http://www.nextdom.org/blog" class="text-blue">{{Accéder au blog}}</a></p>
 				</div>
 				<!-- /.box-body -->
 			</div>
