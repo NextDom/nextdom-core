3e20803fa6fb1f3d7999fbaf7f37b117e1afcb60
Unknown <vincent.fresnel@gmail.com>
2018-10-15 13:31:05 +0200

    fix name page

--- assets/js/adminlte/utils.js
+++ assets/js/adminlte/utils.js
@@ -3,7 +3,7 @@ $(function() {
     $(":input").inputmask();
     $('[data-toggle="tooltip"]').tooltip();
 
-    var y = location.search.substring(1).split('p=');
+    var y = location.search.split("&")[1].split('p=');
     if( y[1] == 'dashboard'){
         document.getElementById("filter_category").style.display = "block";
         document.getElementById("bt_editDashboardWidgetOrder").style.display = "block";
