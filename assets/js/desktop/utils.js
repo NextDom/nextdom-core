661e30c6f3fd219b39a621ee9ca12190cf1c18fd
cyrilphoenix <cyrilphoenix71@gmail.com>
2018-10-24 22:51:08 +0200

    fix

--- assets/js/desktop/utils.js
+++ assets/js/desktop/utils.js
@@ -1032,10 +1032,6 @@ function toggleFullScreen() {
     }
 }
 
-$('#bt_goOnTop').click(function() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-});
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
