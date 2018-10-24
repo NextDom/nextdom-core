363a0d40f12fac3de1c1aa57e91d628b444a7218
Unknown <vincent.fresnel@gmail.com>
2018-10-24 16:40:40 +0200

    issue https://github.com/Sylvaner/nextdom-core/issues/422

--- assets/js/desktop/profils.js
+++ assets/js/desktop/profils.js
@@ -174,3 +174,10 @@ $('#user_avatar').fileupload({
         }
     }
 });
+
+$(".Avatar").on('click', function (event) {
+    console.log($(this).attr('src'));
+    $('.userAttr[data-l2key=avatar]').value($(this).attr('src'));
+    $('#monAvatar').attr('src',$(this).attr('src'));
+    notify("{{Profil}}", '{{Image changée}}', 'success');
+});
