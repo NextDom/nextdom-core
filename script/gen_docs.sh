2145f409b0bd9368501c7bdb1d53a8f9388f3e5f
Slobberbone <slobberbone4884@gmail.com>
2018-10-30 17:13:11 +0100

    Auto generated NextDom documentation
    https://github.com/Sylvaner/nextdom-core/issues/491

---
+++ script/gen_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+../vendor/bin/phpdox -f phpdox/phpdox.xml
+../vendor/bin/phpmetrics --report-html=../docs/report ../
+ 
+sed -i '/<a href="source\/index.xhtml">Source<\/a>/r phpdox/addReport.txt'  ../docs/html/index.xhtml 
+sed -i '/<a href="source\/index.xhtml">Source<\/a>/r phpdox/addReport.txt'  ../docs/html/namespaces.xhtml
+sed -i '/<a href="source\/index.xhtml">Source<\/a>/r phpdox/addReport.txt'  ../docs/html/interfaces.xhtml
+sed -i '/<a href="source\/index.xhtml">Source<\/a>/r phpdox/addReport.txt'  ../docs/html/classes.xhtml
+if [ ! -e ../docs/index.xhtml ]; then
+	ln -s ../docs/html/index.xhtml ../docs/index.xhtml
+fi
+
