99148d631d26cfb433af2738511ef0e109afd6bf
Slobberbone <slobberbone4884@gmail.com>
2018-10-31 11:32:56 +0100

    Create composer project for docs & reports

--- scripts/gen_docs.sh
+++ scripts/gen_docs.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-../vendor/bin/phpdox -f phpdox/phpdox.xml
-../vendor/bin/phpmetrics --report-html=../docs/report ../
+cd phpdox
+composer update
+cd ..
+
+./phpdox/vendor/bin/phpdox -f phpdox/phpdox.xml
+./phpdox/vendor/bin/phpmetrics --report-html=../docs/report ../
  
 sed -i '/<a href="source\/index.xhtml">Source<\/a>/r phpdox/addReport.txt'  ../docs/html/index.xhtml 
 sed -i '/<a href="source\/index.xhtml">Source<\/a>/r phpdox/addReport.txt'  ../docs/html/namespaces.xhtml
