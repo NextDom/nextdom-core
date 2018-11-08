15428bb2a7f2e2701c0a5e4bd5b1d41ceca659ba
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-11-08 07:10:52 +0100

    Class event/EventManager
    Clarification de la gestion des évènements pour une meilleure compréhension et permettre le debug.

--- core/class/event.class.php
+++ core/class/event.class.php
@@ -16,152 +16,58 @@
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* * ***************************Includes********************************* */
+/* This file is part of NextDom Software.
+ *
+ * NextDom Software is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NextDom Software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NextDom Software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 require_once __DIR__ . '/../../core/php/core.inc.php';
 
+use NextDom\Managers\EventManager;
+
+/**
+ * Class event
+ *
+ * Interface for jeedom plugins compatibility
+ * @see NextDom\Managers\EventManager
+ */
 class event {
-    /*     * *************************Attributs****************************** */
-
-    private static $limit = 250;
-    private static $_fd = null;
-
-    /*     * ***********************Methode static*************************** */
-
-    public static function getFileDescriptorLock() {
-        if (self::$_fd === null) {
-            self::$_fd = fopen(nextdom::getTmpFolder() . '/event_cache_lock', 'w');
-            chmod(nextdom::getTmpFolder() . '/event_cache_lock', 0777);
-        }
-        return self::$_fd;
-    }
-
+    /**
+     * @see EventManager::add
+     */
     public static function add($_event, $_option = array()) {
-        $waitIfLocked = true;
-        $fd = self::getFileDescriptorLock();
-        if (flock($fd, LOCK_EX, $waitIfLocked)) {
-            $cache = cache::byKey('event');
-            $value = json_decode($cache->getValue('[]'), true);
-            if (!is_array($value)) {
-                $value = array();
-            }
-            $value[] = array('datetime' => getmicrotime(), 'name' => $_event, 'option' => $_option);
-            cache::set('event', json_encode(self::cleanEvent($value)));
-            flock($fd, LOCK_UN);
-        }
+        EventManager::add($_event, $_option);
     }
 
+    /**
+     * @see EventManager::adds
+     */
     public static function adds($_event, $_values = array()) {
-        $waitIfLocked = true;
-        $fd = self::getFileDescriptorLock();
-        if (flock($fd, LOCK_EX, $waitIfLocked)) {
-            $cache = cache::byKey('event');
-            $value_src = json_decode($cache->getValue('[]'), true);
-            if (!is_array($value_src)) {
-                $value_src = array();
-            }
-            $value = array();
-            foreach ($_values as $option) {
-                $value[] = array('datetime' => getmicrotime(), 'name' => $_event, 'option' => $option);
-            }
-            cache::set('event', json_encode(self::cleanEvent(array_merge($value_src, $value))));
-            flock($fd, LOCK_UN);
-        }
+        EventManager::adds($_event, $_values);
     }
 
-    public static function cleanEvent($_events) {
-        $_events = array_slice(array_values($_events), -self::$limit, self::$limit);
-        $manage_event = array('eqLogic::update', 'cmd::update', 'scenario::update', 'jeeObject::summary::update');
-        $return = array();
-        foreach ($_events as $event) {
-            if (!in_array($event['name'], $manage_event)) {
-                $return[] = $event;
-                continue;
-            }
-            if ($event['name'] == 'eqLogic::update') {
-                $return[$event['name'] . '::' . $event['option']['eqLogic_id']] = $event;
-            }
-            if ($event['name'] == 'cmd::update') {
-                $return[$event['name'] . '::' . $event['option']['cmd_id']] = $event;
-            }
-            if ($event['name'] == 'scenario::update') {
-                $return[$event['name'] . '::' . $event['option']['scenario_id']] = $event;
-            }
-            if ($event['name'] == 'jeeObject::summary::update') {
-                $return[$event['name'] . '::' . $event['option']['object_id']] = $event;
-            }
-        }
-        usort($return, 'self::orderEvent');
-        return $return;
-    }
-
+    /**
+     * @see EventManager::orderEvent
+     */
     public static function orderEvent($a, $b) {
-        if ($a['datetime'] == $b['datetime']) {
-            return 0;
-        }
-        return ($a['datetime'] < $b['datetime']) ? -1 : 1;
+        return EventManager::orderEvent($a, $b);
     }
 
+    /**
+     * @see EventManager::changes
+     */
     public static function changes($_datetime, $_longPolling = null, $_filter = null) {
-        $return = self::filterEvent(self::changesSince($_datetime), $_filter);
-        if ($_longPolling === null || count($return['result']) > 0) {
-            return $return;
-        }
-        $waitTime = config::byKey('event::waitPollingTime');
-        $i = 0;
-        $max_cycle = $_longPolling / $waitTime;
-        while (count($return['result']) == 0 && $i < $max_cycle) {
-            if ($waitTime < 1) {
-                usleep(1000000 * $waitTime);
-            } else {
-                sleep(round($waitTime));
-            }
-            sleep(1);
-            $return = self::filterEvent(self::changesSince($_datetime), $_filter);
-            $i++;
-        }
-        return $return;
+        return EventManager::changes($_datetime, $_longPolling, $_filter);
     }
-
-    private static function filterEvent($_data = array(), $_filter = null) {
-        if ($_filter == null) {
-            return $_data;
-        }
-        $filters = cache::byKey($_filter . '::event')->getValue(array());
-        $return = array('datetime' => $_data['datetime'], 'result' => array());
-        foreach ($_data['result'] as $value) {
-            if (isset($_filter::$_listenEvents) && !in_array($value['name'], $_filter::$_listenEvents)) {
-                continue;
-            }
-            if (count($filters) != 0 && $value['name'] == 'cmd::update' && !in_array($value['option']['cmd_id'], $filters)) {
-                continue;
-            }
-            $return['result'][] = $value;
-        }
-        return $return;
-    }
-
-    private static function changesSince($_datetime) {
-        $return = array('datetime' => $_datetime, 'result' => array());
-        $cache = cache::byKey('event');
-        $events = json_decode($cache->getValue('[]'), true);
-        if (!is_array($events)) {
-            $events = array();
-        }
-        $values = array_reverse($events);
-        if (count($values) > 0) {
-            $return['datetime'] = $values[0]['datetime'];
-            foreach ($values as $value) {
-                if ($value['datetime'] <= $_datetime) {
-                    break;
-                }
-                $return['result'][] = $value;
-            }
-        }
-        $return['result'] = array_reverse($return['result']);
-        return $return;
-    }
-
-    /*     * *********************Methode d'instance************************* */
-
-    /*     * **********************Getteur Setteur*************************** */
 }
---
+++ src/Managers/EventManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sylvain
+ * Date: 07/11/2018
+ * Time: 08:16
+ */
+
+namespace NextDom\Managers;
+
+
+class EventManager
+{
+
+}
