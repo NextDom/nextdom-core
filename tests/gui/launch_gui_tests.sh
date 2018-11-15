1cb53418bc836bb71f8885d0d1aad4f1eaa0590a
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-11-15 17:37:04 +0100

    Script de lancement de l'ensemble des tests

---
+++ tests/gui/launch_gui_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ "$#" -ne 3 ]; then
+  read -p "Adresse du nextdom : " URL
+  read -p "Utilisateur : " LOGIN
+  read -p "Mot de passe : " PASSWORD
+else
+  URL=$1
+  LOGIN=$2
+  PASSWORD=$3
+fi
+
+if echo "$URL" | grep -qv "http"
+then
+  URL="http://$URL"
+fi
+
+echo "Test on page $URL"
+echo ">>> Connect page <<<"
+python3 -W ignore connection_page.py "$URL" "$LOGIN" "$PASSWORD"
+echo ">>> Administration pages <<<"
+python3 -W ignore administrations_page.py "$URL" "$LOGIN" "$PASSWORD"
+echo ">>> Rescue mode <<<"
+python3 -W ignore rescue_page.py "$URL" "$LOGIN" "$PASSWORD"
