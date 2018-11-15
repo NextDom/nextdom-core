ba50b7dae43d79525047876ada14dbb73cb82743
Unknown <vincent.fresnel@gmail.com>
2018-11-15 09:35:03 +0100

    modif gen_asset

--- src/Helpers/PrepareView.php
+++ src/Helpers/PrepareView.php
@@ -338,14 +338,12 @@ class PrepareView
             $pageData['JS_POOL'][] = '/vendor/node_modules/autosize/dist/autosize.js';
             $pageData['JS_POOL'][] = '/vendor/node_modules/tablesorter/dist/js/jquery.tablesorter.min.js';
             $pageData['JS_POOL'][] = '/vendor/node_modules/tablesorter/dist/js/jquery.tablesorter.widgets.min.js';
-            $pageData['JS_END_POOL'][] = '/vendor/node_modules/admin-lte/dist/js/adminlte.min.js';
-            $pageData['JS_END_POOL'][] = '/public/js/adminlte/adminlte_nextdom.js';
         } else {
             $pageData['JS_POOL'][] = 'vendor/node_modules/jquery-ui-dist/jquery-ui.min.js';
             $pageData['JS_POOL'][] = '/vendor/node_modules/bootstrap/dist/js/bootstrap.min.js';
             $pageData['JS_POOL'][] = '/vendor/node_modules/admin-lte/dist/js/adminlte.min.js';
             $pageData['JS_POOL'][] = '/public/js/adminlte/adminlte_nextdom.js';
-            $pageData['JS_POOL'][] = '/assets/3rdparty/jquery.utils/jquery.utils.js';
+            $pageData['JS_POOL'][] = '/public/3rdparty/jquery.utils/jquery.utils.js';
             $pageData['JS_POOL'][] = '/vendor/node_modules/izitoast/dist/js/iziToast.min.js';
             $pageData['JS_POOL'][] = '/public/js/desktop/utils.js';
             $pageData['JS_POOL'][] = '/core/js/core.js';
