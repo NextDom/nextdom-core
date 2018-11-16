4543c11f8ecf50635177312b2543334936979f5a
colonelMoutarde <sanchezluc@gmail.com>
2018-11-16 19:23:43 +0100

    testing for create real Controller

--- src/Helpers/PagesController.php
+++ src/Helpers/PagesController.php
@@ -45,55 +45,55 @@ use NextDom\Managers\CacheManager;
 class PagesController
 {
     const routesList = [
-        'dashboard' => 'dashboard',
-        'scenario' => 'scenario',
+        'dashboard'      => 'dashboard',
+        'scenario'       => 'scenario',
         'administration' => 'administration',
-        'backup' => 'backup',
-        'object' => 'object',
-        'message' => 'message',
-        'cron' => 'cron',
-        'user' => 'user',
-        'update' => 'update',
-        'system' => 'system',
-        'database' => 'database',
-        'display' => 'display',
-        'log' => 'log',
-        'report' => 'report',
-        'plugin' => 'plugin',
-        'editor' => 'editor',
-        'migration' => 'migration',
-        'history' => 'history',
-        'shutdown' => 'shutdown',
-        'health' => 'health',
-        'profils' => 'profils',
-        'view' => 'view',
-        'view_edit' => 'viewEdit',
-        'eqAnalyse' => 'eqAnalyze',
-        'plan' => 'plan',
-        'plan3d' => 'plan3d',
-        'market' => 'market',
-        'reboot' => 'reboot',
-        'network' => 'network',
-        'cache' => 'cache',
-        'general' => 'general',
-        'log_admin' => 'logAdmin',
-        'realtime' => 'realtime',
-        'custom' => 'custom',
-        'api' => 'API',
-        'commandes' => 'commandes',
-        'osdb' => 'osdb',
-        'reports_admin' => 'reports_admin',
-        'eqlogic' => 'eqlogic',
-        'interact' => 'interact',
+        'backup'         => 'backup',
+        'object'         => 'object',
+        'message'        => 'message',
+        'cron'           => 'cron',
+        'user'           => 'user',
+        'update'         => 'update',
+        'system'         => 'system',
+        'database'       => 'database',
+        'display'        => 'display',
+        'log'            => 'log',
+        'report'         => 'report',
+        'plugin'         => 'plugin',
+        'editor'         => 'editor',
+        'migration'      => 'migration',
+        'history'        => 'history',
+        'shutdown'       => 'shutdown',
+        'health'         => 'health',
+        'profils'        => 'profils',
+        'view'           => 'view',
+        'view_edit'      => 'viewEdit',
+        'eqAnalyse'      => 'eqAnalyze',
+        'plan'           => 'plan',
+        'plan3d'         => 'plan3d',
+        'market'         => 'market',
+        'reboot'         => 'reboot',
+        'network'        => 'network',
+        'cache'          => 'cache',
+        'general'        => 'general',
+        'log_admin'      => 'logAdmin',
+        'realtime'       => 'realtime',
+        'custom'         => 'custom',
+        'api'            => 'API',
+        'commandes'      => 'commandes',
+        'osdb'           => 'osdb',
+        'reports_admin'  => 'reports_admin',
+        'eqlogic'        => 'eqlogic',
+        'interact'       => 'interact',
         'interact_admin' => 'interact_admin',
-        'links' => 'links',
-        'security' => 'security',
-        'summary' => 'summary',
-        'update_admin' => 'update_admin',
-        'users' => 'users',
-        'tools' => 'tools',
-        'note' => 'note',
-        'pluginRoute' => 'pluginRoute'
+        'links'          => 'links',
+        'security'       => 'security',
+        'summary'        => 'summary',
+        'update_admin'   => 'update_admin',
+        'users'          => 'users',
+        'tools'          => 'tools',
+        'note'           => 'note',
+        'pluginRoute'    => 'pluginRoute'
     ];
 
     /**
@@ -114,68 +114,7 @@ class PagesController
         return $route;
     }
 
-    /**
-     * Render dashboard
-     *
-     * @param Render $render Render engine
-     * @param array $pageContent Page data
-     *
-     * @return string Content of Dashboard V2 page
-     *
-     * @throws \NextDom\Exceptions\CoreException
-     * @throws \Twig_Error_Loader
-     * @throws \Twig_Error_Runtime
-     * @throws \Twig_Error_Syntax
-     */
-    public static function dashboard(Render $render, array &$pageContent): string
-    {
-        Status::initConnectState();
-        Status::isConnectedAdminOrFail();
-
-        $pageContent['JS_VARS']['nextdom_Welcome'] = \config::byKey('nextdom::Welcome');
-        $pageContent['JS_VARS']['SEL_OBJECT_ID'] = Utils::init('object_id');
-        $pageContent['JS_VARS']['SEL_CATEGORY'] = Utils::init('category', 'all');
-        $pageContent['JS_VARS']['SEL_TAG'] = Utils::init('tag', 'all');
-        $pageContent['JS_VARS']['SEL_SUMMARY'] = Utils::init('summary');
-
-        if ($pageContent['JS_VARS']['SEL_OBJECT_ID'] == '') {
-            $object = JeeObjectManager::byId($_SESSION['user']->getOptions('defaultDashboardObject'));
-        } else {
-            $object = JeeObjectManager::byId(Utils::init('object_id'));
-        }
-
-        if (!is_object($object)) {
-            $object = JeeObjectManager::rootObject();
-        }
-
-        if (!is_object($object)) {
-            throw new \Exception(__('Aucun objet racine trouvé. Pour en créer un, allez dans Outils -> <a href="/index.php?v=d&p=object">Objets</a>'));
-        }
-        $pageContent['JS_VARS']['rootObjectId'] = $object->getId();
-
-        $pageContent['dashboardDisplayObjectByDefault'] = $_SESSION['user']->getOptions('displayObjetByDefault');
-        $pageContent['dashboardDisplayScenarioByDefault'] = $_SESSION['user']->getOptions('displayScenarioByDefault');
-        $pageContent['dashboardCategory'] = $pageContent['JS_VARS']['SEL_CATEGORY'];
-        $pageContent['dashboardTag'] = $pageContent['JS_VARS']['SEL_TAG'];
-        $pageContent['dashboardCategories'] = \nextdom::getConfiguration('eqLogic:category', true);
-        $pageContent['dashboardTags'] = EqLogicManager::getAllTags();
-        $pageContent['dashboardObjectId'] = $pageContent['JS_VARS']['SEL_OBJECT_ID'];
-        $pageContent['dashboardObject'] = $object;
-        $pageContent['dashboardChildrenObjects'] = JeeObjectManager::buildTree($object);
-        $pageContent['profilsUser'] = $_SESSION['user'];
-
-        if ($pageContent['dashboardDisplayScenarioByDefault'] == 1) {
-            $pageContent['dashboardScenarios'] = ScenarioManager::all();
-        }
-        $pageContent['JS_POOL'][] = '/public/js/desktop/dashboard.js';
-        $pageContent['JS_END_POOL'][] = '/public/js/desktop/dashboard_events.js';
-        // A remettre une fois mise sous forme de thème//
-        $pageContent['JS_POOL'][] = '/vendor/node_modules/isotope-layout/dist/isotope.pkgd.min.js';
-        $pageContent['JS_POOL'][] = '/assets/3rdparty/jquery.multi-column-select/multi-column-select.js';
-        $pageContent['JS_END_POOL'][] = '/public/js/adminlte/utils.js';
-
-        return $render->get('/desktop/dashboard.html.twig', $pageContent);
-    }
+    
 
     /**
      * Render scenario page
