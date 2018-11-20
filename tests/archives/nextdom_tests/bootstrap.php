8286b0d638dd29b3c187a82ff0f69f9486575021
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-11-20 06:34:30 +0100

    Auto stash before merge of "develop" and "origin/develop"
    Tests gui

---
+++ tests/archives/nextdom_tests/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+echo "Test de NextDom\n";
+?>
---
+++ tests/install_env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pip3 install selenium
+
+if [ "$(uname)" == "Darwin" ]; then
+    curl -s https://chromedriver.storage.googleapis.com/2.43/chromedriver_mac64.zip > chromedriver.zip
+else
+    curl -s https://chromedriver.storage.googleapis.com/2.43/chromedriver_linux64.zip > chromedriver.zip
+fi
+unzip -a chromedriver.zip
+cp -fr chromedriver gui/chromedriver
+chmod +x gui/chromedriver
+rm -fr chromedriver
+rm -fr chromedriver.zip
