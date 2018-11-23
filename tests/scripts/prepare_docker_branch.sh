8ff218898f7e747f45bb0da9960db57980079ebc
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-11-23 13:10:38 +0100

    Possibilité de tester une branch

---
+++ tests/scripts/prepare_docker_branch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+	echo "Le nom de la branche doit être indiquée"
+	exit 1
+fi
+rm -fr /tmp/nextdom-core
+cd /tmp
+echo "Clone repo"
+git clone https://github.com/Sylvaner/nextdom-core
+echo "Change branch"
+cd nextdom-core
+git checkout $1
+docker run -d -p 8765:80 -v `pwd`:/data --name="nextdom-test" sylvaner1664/nextdom-test:latest > /dev/null 2>&1
+END_OF_INSTALL_STR="OK NEXTDOM TEST READY"
+
+while true
+do
+	DOCKER_LOGS=$(docker logs nextdom-test 2>&1)
+	if [[ "$DOCKER_LOGS" =~ .*OK.NEXTDOM.TEST.READY.* ]]; then
+		break
+	fi
+	sleep 10
+done
+docker commit nextdom-test nextdom-test-snap
+docker kill nextdom-test
+docker rm nextdom-test
