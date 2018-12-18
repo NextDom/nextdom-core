7fe664b3f7da8c8ba7d91733c87fd3e5fc061a59
Sylvain Dangin <sylvain.dangin@gmail.com>
2018-12-18 07:27:47 +0100

    Fix Config and tests

--- tests/gui/custom_js_css.py
+++ tests/gui/custom_js_css.py
@@ -10,13 +10,14 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class MigrationPage(unittest.TestCase):
-    """Test all pages linked in administration page
+class CustomJsCssPage(unittest.TestCase):
+    """Test customization Javascript and CSS from user
     """
     driver = None
     url = None
     login = None
     password = None
+    CUSTOM_PAGE = 'index.php?v=d&p=custom'
 
     @classmethod
     def setUpClass(cls):
@@ -28,9 +29,7 @@ class MigrationPage(unittest.TestCase):
             cls.driver.get(cls.url)
             cls.connect_to_nextdom()
         except WebDriverException as err:
-            print(err.keys())
-            print(err['code'])
-            if err['code'] == -32000:
+            if err.code == -32000:
                 print('Impossible to access to '+cls.url)
             else:
                 print("Chromedriver needed to run tests on Chrome.")
@@ -55,17 +54,23 @@ class MigrationPage(unittest.TestCase):
         # Wait animation ending
         sleep(4)
 
-    def test_dashboard_page(self):
-        self.driver.get(self.url+'index.php?v=d&p=dashboard')
-        sleep(4)
-        self.assertIsNotNone(self.driver.page_source.find('Maison'))
+    def test_add_custom_js(self):
+        self.driver.get(self.url+self.CUSTOM_PAGE)
+        # Wait for loading page
+        sleep(2)
+        # Add javascript
+        system_menu = self.driver.find_element_by_css_selector('a[href="#advanced"]')
+        system_menu.click()
+        js_editor = self.driver.find_element_by_id('ta_jsDesktopContent')
+        js_editor.send_keys('console.log(\'just_a_test\');')
+        save_button = js_editor = self.driver.find_element_by_id('bt_savecustom')
+        save_button.click();
+        # Reload the page
+        self.driver.get(self.url+self.CUSTOM_PAGE)
+        shell_output = subprocess.check_output(["docker", "exec", "-it", "nextdom-test-custom-js-css", "rm", "-f", "/var/www/html/var/custom/custom.js"])
+        self.assertIsNotNone(system_menu)
         self.assertEqual(0, len(self.driver.get_log('browser')))
-
-    def test_dashboard_page(self):
-        self.driver.get(self.url+'index.php?v=d&p=backup')
-        sleep(4)
-        self.assertIsNotNone(self.driver.find_element_by_css_selector('option[value="/var/www/html/backup/backup-Jeedom-3.2.11-2018-11-17-23h26.tar.gz"]'))
-        self.assertEqual(0, len(self.driver.get_log('browser')))
+        print(self.driver.get_log('browser'))
 
 # Entry point
 if __name__ == "__main__":
@@ -73,11 +78,11 @@ if __name__ == "__main__":
         print('Usage : ' + sys.argv[0]+ ' url login password')
     else:
         if sys.argv[1][:-1] == '/':
-            MigrationPage.url = sys.argv[1]
+            RescuePage.url = sys.argv[1]
         else:
-            MigrationPage.url = sys.argv[1]+'/'
-        MigrationPage.login = sys.argv[2]
-        MigrationPage.password = sys.argv[3]
+            RescuePage.url = sys.argv[1]+'/'
+        RescuePage.login = sys.argv[2]
+        RescuePage.password = sys.argv[3]
         # unittest use sys.argv
         del sys.argv[1:]
         # failfast=True pour arrêter à la première erreur
