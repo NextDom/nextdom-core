6dc9dbb0c9d26860980cbb9b84112667478f0aaa
Slobberbone <slobberbone4884@gmail.com>
2019-04-06 13:33:30 +0200

    Add MigrationHelper
    Add an helper to manage migration stuff from backup or from previous version

---
+++ src/Enums/FoldersReferential.php
@@ -0,0 +1,27 @@
+<?php
+
+/* This file is part of NextDom.
+ *
+ * NextDom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NextDom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NextDom. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace NextDom\Enums;
+
+class FoldersReferential extends Enum
+{
+
+    const NEXTDOMFOLDERS = ['assets','backup','core','data','docs','install','log','mobile','plugins','public','scripts','src','tests','translations','vendor','views'];
+    const NEXTDOMFILES = ['cache.tar.gz' ,'composer.json','composer.lock','COPYING','DB_backup.sql','favicon.ico','index.php','LICENSE','manifest.json','manifest.webmanifest','mobile.manifest.php','package.json','phpunit.xml.dist','README.md','robots.txt'];
+
+}
