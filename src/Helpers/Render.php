17b09ad841158d0fa24950ef47f97272e5616027
Sylvain Dangin <sylvain.dangin@gmail.com>
2019-04-21 12:18:54 +0200

    PHPdoc and optims
    PHPdoc, optims and return methodes
    
    PHPdoc, optims and return methodes
    
    PHPdoc
    
    PHPdoc
    
    PHPdoc and return values
    
    Test if true is strict

--- src/Helpers/Render.php
+++ src/Helpers/Render.php
@@ -56,7 +56,10 @@ class Render
         $this->initRenderer();
     }
 
-    private function initTranslation($language)
+    /**
+     * @param $language
+     */
+    private function initTranslation(string $language)
     {
         $this->translator = new Translator($language, null, NEXTDOM_ROOT . '/var/cache/i18n');
         $this->translator->addLoader('yaml', new YamlFileLoader());
