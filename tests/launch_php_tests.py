29a777f39c732fdf03353134f8d7538d59970637
Sylvain Dangin <sylvain.dangin@gmail.com>
2019-05-15 18:31:10 +0200

    Fix du bug de l'image des plan
    Quelques corrections sur la page plan
     - Upload du plan
     - Erreur javascript
    Upload des fichiers jpeg en général
    Un test complémentaire pour la page des scénarios

--- tests/launch_php_tests.py
+++ tests/launch_php_tests.py
@@ -17,7 +17,7 @@ def php_tests():
     start_test_container(container_name, NEXTDOM_PASSWORD)
     exec_command_in_container(container_name, '/bin/cp -fr /var/www/html/tests/data/plugin4tests /var/www/html/plugins')
     exec_command_in_container(container_name, '/bin/chown www-data:www-data -R /var/www/html/plugins')
-    exec_command_in_container(container_name, 'bash -c "mysql -u root nextdomdev < /var/www/html/tests/data/phpunit_tests_fixtures.sql"')
+    exec_command_in_container(container_name, 'bash -c "mysql -u root nextdomdev < /var/www/html/tests/data/tests_fixtures.sql"')
     exec_command_in_container(container_name, 'service cron stop > /dev/null')
     exec_command_in_container(container_name, 'apt-get install -y php-xdebug > /dev/null 2>&1')
     exec_command_in_container(container_name, 'bash -c "cd /var/www/html && vendor/bin/phpunit --configuration tests/phpunit_tests/phpunit.xml --testsuite AllTests"')
