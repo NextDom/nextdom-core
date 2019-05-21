2de50ec91b809db04b7434c6927572484ede03c8
Sylvain Dangin <sylvain.dangin@gmail.com>
2019-05-21 22:13:41 +0200

    Version testable

--- scripts/prepare_mobile.sh
+++ scripts/prepare_mobile.sh
@@ -4,4 +4,5 @@ ROOT_DIR=$(dirname $(cd $(dirname $0) && pwd -P))
 cd $ROOT_DIR/src/mobile
 ln -s $ROOT_DIR/assets/icon $ROOT_DIR/src/mobile/src/assets/icons
 npm install
-npm run serve
+npm run build
+mv $ROOT_DIR/src/mobile/dist $ROOT_DIR/mobile
