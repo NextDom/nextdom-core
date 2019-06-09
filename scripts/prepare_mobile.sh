7037a5f7ba965bed4a570220e3134863b101fe4b
Sylvain Dangin <sylvain.dangin@gmail.com>
2019-06-09 16:01:50 +0200

    Add mobile in postinst

--- scripts/prepare_mobile.sh
+++ scripts/prepare_mobile.sh
@@ -5,4 +5,4 @@ cd $ROOT_DIR/src/mobile
 ln -s $ROOT_DIR/assets/icon $ROOT_DIR/src/mobile/src/assets/icons
 npm install
 npm run build
-mv $ROOT_DIR/src/mobile/dist $ROOT_DIR/mobile
+mv $ROOT_DIR/src/mobile/dist/* $ROOT_DIR/mobile
