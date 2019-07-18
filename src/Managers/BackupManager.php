479707854c870d6d63113c2de7c80ed2f4f1f8b5
Slobberbone <slobberbone4884@gmail.com>
2019-07-18 14:52:32 +0200

    Fix merge

--- src/Managers/BackupManager.php
+++ src/Managers/BackupManager.php
@@ -1,35 +1,18 @@
-
-/*
-* This file is part of the NextDom software (https://github.com/NextDom or http://nextdom.github.io).
-* Copyright (c) 2018 NextDom.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, version 2.
-*
-* This program is distributed in the hope that it will be useful, but
-* WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-* General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with this program. If not, see <http://www.gnu.org/licenses/>.
-*/
-
-/* This file is part of Jeedom.
+<?php
+/* This file is part of NextDom Software.
  *
- * Jeedom is free software: you can redistribute it and/or modify
+ * NextDom is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Jeedom is distributed in the hope that it will be useful,
+ * NextDom Software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ * along with NextDom Software. If not, see <http://www.gnu.org/licenses/>.
  */
 
 namespace NextDom\Managers;
@@ -42,7 +25,6 @@ use NextDom\Enums\FoldersReferential;
 use NextDom\Helpers\MigrationHelper;
 use NextDom\Helpers\NextDomHelper;
 use NextDom\Helpers\SystemHelper;
-use NextDom\Helpers\Utils;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 use splitbrain\PHPArchive\Tar;
