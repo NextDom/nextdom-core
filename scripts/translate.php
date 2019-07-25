5c5a17020afa5d3481d11bec87117653c4e80513
Sylvain Dangin <sylvain.dangin@gmail.com>
2019-07-25 11:12:30 +0200

    Fix translate.php script

--- scripts/translate.php
+++ scripts/translate.php
@@ -7,9 +7,13 @@ if (count($argv) == 1) {
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define('NEXTDOM_ROOT', __DIR__ . '/../');
+define('NEXTDOM_ROOT', realpath(__DIR__ . '/..'));
 define('NEXTDOM_DATA', '/tmp/');
 
+// Avoid scripts warning during tests
+if (!is_dir(NEXTDOM_ROOT . '/plugins')) {
+    mkdir(NEXTDOM_ROOT . '/plugins');
+}
 \NextDom\Helpers\TranslateHelper::setLanguage('fr_FR');
 
 $target = $argv[1];
