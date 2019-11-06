7c22421e523dd5f94da4838999db702cfdd438de
Unknown <vincent.fresnel@gmail.com>
2019-11-06 14:48:12 +0100

    delete realtime modal

--- assets/js/desktop/diagnostic/log.js
+++ assets/js/desktop/diagnostic/log.js
@@ -1,4 +1,4 @@
-/* This file is part of Jeedom.
+* This file is part of Jeedom.
 *
 * Jeedom is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
