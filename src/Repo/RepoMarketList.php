42e423a4834729aa6e7d23804ec0f75cc43ba3eb
Sylvaner <sylvain.dangin@gmail.com>
2019-11-08 14:27:06 +0100

    Clear sonarcloud bugs

--- src/Repo/RepoMarketList.php
+++ src/Repo/RepoMarketList.php
@@ -70,10 +70,10 @@ function displayWidgetName($_name)
     }
     switch ($name[1]) {
         case 'info':
-            $result .= '<i class="fa fa-eye fa-fw" title="{{Widget de type information}}"></i> ';
+            $result .= '<i aria-hidden="true" class="fa fa-eye fa-fw" title="{{Widget de type information}}"></i> ';
             break;
         case 'action':
-            $result .= '<i class="fa fa-exclamation-circle fa-fw" title="{{Widget de type action}}"></i> ';
+            $result .= '<i aria-hidden="true" class="fa fa-exclamation-circle fa-fw" title="{{Widget de type action}}"></i> ';
             break;
         default:
             $result .= $name[1];
@@ -114,10 +114,10 @@ function displayWidgetType($_name)
     }
     switch ($name[1]) {
         case 'info':
-            $result .= '<i class="fa fa-eye fa-fw" title="Widget de type information" style="position: absolute;top: 31px; left: 15px;"></i> ';
+            $result .= '<i aria-hidden="true" class="fa fa-eye fa-fw" title="Widget de type information" style="position: absolute;top: 31px; left: 15px;"></i> ';
             break;
         case 'action':
-            $result .= '<i class="fa fa-exclamation-circle fa-fw" title="Widget de type action" style="position: absolute;top: 31px; left: 15px;"></i> ';
+            $result .= '<i aria-hidden="true" class="fa fa-exclamation-circle fa-fw" title="Widget de type action" style="position: absolute;top: 31px; left: 15px;"></i> ';
             break;
         default:
             $result .= "";
@@ -166,23 +166,23 @@ function displayWidgetSubtype($_name)
     <div class="action-bar">
         <?php
         if ($name !== null && strpos($name, '$') !== false) {
-            echo '<a class="btn btn-danger btn-action-bar" id="bt_returnMarketList" style="margin-top : 50px;" data-href=' . buildUrl('name', '') . '><i class="fas fa-chevron-left"></i>{{Retour}}</a>';
+            echo '<a class="btn btn-danger btn-action-bar" id="bt_returnMarketList" style="margin-top : 50px;" data-href=' . buildUrl('name', '') . '><i aria-hidden="true" class="fas fa-chevron-left"></i>{{Retour}}</a>';
         }
         ?>
         <?php if (Utils::init('type', 'plugin') == 'plugin') { ?>
             <div class="action-group">
                 <div class="btn-group">
-                    <a class="btn bt_pluginFilter <?php echo (Utils::init('cost') == 'free') ? 'btn-primary' : 'btn-default' ?>" data-href="<?php echo buildUrl('cost', 'free'); ?>"><i class="fas fa-gift"></i><span>{{Gratuit}}</span></a>
-                    <a class="btn bt_pluginFilter <?php echo (Utils::init('cost') == 'paying') ? 'btn-primary' : 'btn-default' ?>" data-href="<?php echo buildUrl('cost', 'paying'); ?>"><i class="fas fa-euro-sign"></i><span>{{Payant}}</span></a>
-                    <a class="btn bt_pluginFilter <?php echo (Utils::init('cost') == '') ? 'btn-primary' : 'btn-default' ?>" data-href="<?php echo buildUrl('cost', ''); ?>"><i class="fas fa-times"></i></a>
+                    <a class="btn bt_pluginFilter <?php echo (Utils::init('cost') == 'free') ? 'btn-primary' : 'btn-default' ?>" data-href="<?php echo buildUrl('cost', 'free'); ?>"><i aria-hidden="true" class="fas fa-gift"></i><span>{{Gratuit}}</span></a>
+                    <a class="btn bt_pluginFilter <?php echo (Utils::init('cost') == 'paying') ? 'btn-primary' : 'btn-default' ?>" data-href="<?php echo buildUrl('cost', 'paying'); ?>"><i aria-hidden="true" class="fas fa-euro-sign"></i><span>{{Payant}}</span></a>
+                    <a class="btn bt_pluginFilter <?php echo (Utils::init('cost') == '') ? 'btn-primary' : 'btn-default' ?>" data-href="<?php echo buildUrl('cost', ''); ?>"><i aria-hidden="true" class="fas fa-times"></i></a>
                 </div>
             </div>
         <?php } ?>
         <div class="action-group">
             <div class="btn-group">
-                <a class="btn btn-default bt_installFilter" data-state="-1"><i class="fas fa-thumbs-up"></i><span>{{Installé}}</span></a>
-                <a class="btn btn-default bt_installFilter" data-state="1"><i class="fas fa-thumbs-down"></i><span>{{Non installé}}</span></a>
-                <a class="btn btn-primary bt_installFilter" data-state="0"><i class="fas fa-times"></i></a>
+                <a class="btn btn-default bt_installFilter" data-state="-1"><i aria-hidden="true" class="fas fa-thumbs-up"></i><span>{{Installé}}</span></a>
+                <a class="btn btn-default bt_installFilter" data-state="1"><i aria-hidden="true" class="fas fa-thumbs-down"></i><span>{{Non installé}}</span></a>
+                <a class="btn btn-primary bt_installFilter" data-state="0"><i aria-hidden="true" class="fas fa-times"></i></a>
             </div>
         </div>
         <div class="action-group">
@@ -228,14 +228,14 @@ function displayWidgetSubtype($_name)
         </div>
         <div class="action-group">
             <div class="input-group">
-                <a class="input-group-addon cursor" id="bt_resetSearch" data-href='<?php echo buildUrl('name', ''); ?>'><i class="fas fa-times"></i></a>
+                <a class="input-group-addon cursor" id="bt_resetSearch" data-href='<?php echo buildUrl('name', ''); ?>'><i aria-hidden="true" class="fas fa-times"></i></a>
                 <input class="form-control" data-href='<?php echo buildUrl('name', ''); ?>' placeholder="{{Rechercher...}}" id="in_search" value="<?php echo $name ?>"/>
-                <a class="input-group-addon cursor" id="bt_search" data-href='<?php echo buildUrl('name', ''); ?>'><i class="fas fa-search"></i></a>
+                <a class="input-group-addon cursor" id="bt_search" data-href='<?php echo buildUrl('name', ''); ?>'><i aria-hidden="true" class="fas fa-search"></i></a>
             </div>
         </div>
         <div class="action-group">
-            <a class="btn btn-action btn-action-bar pull-right" style="display:none;" id="bt_marketCollapse"><i class="fas fa-plus-square"></i>{{Déplier}}</a>
-            <a class="btn btn-action btn-action-bar pull-right" id="bt_marketUncollapse"><i class="fas fa-minus-square"></i>{{Replier}}</a>
+            <a class="btn btn-action btn-action-bar pull-right" style="display:none;" id="bt_marketCollapse"><i aria-hidden="true" class="fas fa-plus-square"></i>{{Déplier}}</a>
+            <a class="btn btn-action btn-action-bar pull-right" id="bt_marketUncollapse"><i aria-hidden="true" class="fas fa-minus-square"></i>{{Replier}}</a>
         </div>
     </div>
 </section>
@@ -244,7 +244,7 @@ function displayWidgetSubtype($_name)
     <div class="box">
         <div class="box-header with-border">
             <?php
-            echo '<h3 class="box-title"><i class="fas fa-shopping-cart"></i>';
+            echo '<h3 class="box-title"><i aria-hidden="true" class="fas fa-shopping-cart"></i>';
             if ($type == "") {
                 echo '{{Market Jeedom}}</h3>';
             } else {
@@ -254,9 +254,9 @@ function displayWidgetSubtype($_name)
                 echo '<span class="label label-info badge pull-right">' . ConfigManager::byKey('market::username');
                 try {
                     RepoMarket::test();
-                    echo '<i class="fas fa-check spacing-left"></i>';
+                    echo '<i aria-hidden="true" class="fas fa-check spacing-left"></i>';
                 } catch (\Exception $e) {
-                    echo '<i class="fas fa-times spacing-left"></i>';
+                    echo '<i aria-hidden="true" class="fas fa-times spacing-left"></i>';
                 }
                 echo '</span>';
             }
@@ -267,7 +267,7 @@ function displayWidgetSubtype($_name)
             if (count($markets) >= $searchLimit) {
                 echo '<div>';
                 echo '<span class="alert alert-warning market-limited">{{Attention l\'affichage est limité à }}' . $searchLimit . ' {{résultats, utilisez les filtres ou la recherche si ce que vous cherchez n\'apparaît pas...}}</span>';
-                echo '<a class="btn btn-action pull-right" id="bt_resetSearchLimit" data-href="' . buildUrl('limit', '0') .'"><i class="fas fa-times"></i>{{Sans limites}}</a>';
+                echo '<a class="btn btn-action pull-right" id="bt_resetSearchLimit" data-href="' . buildUrl('limit', '0') .'"><i aria-hidden="true" class="fas fa-times"></i>{{Sans limites}}</a>';
                 echo '</div>';
             } else {
                 echo '<div><span class="market-unlimited">' . count($markets);
@@ -278,7 +278,7 @@ function displayWidgetSubtype($_name)
                 }
                 echo ' {{disponibles dans cette catégorie...}}</span>';
                 if (count($markets) >= 100) {
-                    echo '<a class="btn btn-action pull-right" id="bt_SearchLimit" data-href="' . buildUrl('limit', '100') .'"><i class="fas fa-filter"></i>{{Limiter à 100}}</a>';
+                    echo '<a class="btn btn-action pull-right" id="bt_SearchLimit" data-href="' . buildUrl('limit', '100') .'"><i aria-hidden="true" class="fas fa-filter"></i>{{Limiter à 100}}</a>';
                 }
                 echo '</div>';
             }
@@ -312,7 +312,7 @@ function displayWidgetSubtype($_name)
                     echo '<h3 class="box-title">';
                     echo '<span class="accordion-toggle" data-toggle="collapse" data-parent="#accordionPlugin' . $nCategory . '" href="#config_none" style="text-decoration:none;" data-category="' . $nCategory . '">';
                     if (isset($NEXTDOM_INTERNAL_CONFIG['plugin']['category'][$categorie])) {
-                        echo '<i class="fas ' . $NEXTDOM_INTERNAL_CONFIG['plugin']['category'][$categorie]['icon'] . ' spacing-right"></i>' . ucfirst($NEXTDOM_INTERNAL_CONFIG['plugin']['category'][$categorie]['name']) . '</span>';
+                        echo '<i aria-hidden="true" class="fas ' . $NEXTDOM_INTERNAL_CONFIG['plugin']['category'][$categorie]['icon'] . ' spacing-right"></i>' . ucfirst($NEXTDOM_INTERNAL_CONFIG['plugin']['category'][$categorie]['name']) . '</span>';
                     } else {
                         echo ucfirst($categorie) . '</span>';
                     }
@@ -367,7 +367,7 @@ function displayWidgetSubtype($_name)
                     echo '<div class="market-certification market-' . $certificationClass .'">' . strtoupper($market->getCertification()) . '</div>';
                 }
                 if ($install == 'notInstall') {
-                    echo '<i class="fas fa-check market-install"></i>';
+                    echo '<i aria-hidden="true" class="fas fa-check market-install"></i>';
                 }
                 switch ($market->getType()) {
                     case 'widget':
@@ -389,16 +389,16 @@ function displayWidgetSubtype($_name)
                 echo '<span class="market-rating">';
                 for ($i = 1; $i < 6; $i++) {
                     if ($i <= $note) {
-                        echo '<i class="fas fa-star"></i>';
+                        echo '<i aria-hidden="true" class="fas fa-star"></i>';
                     } else {
-                        echo '<i class="far fa-star"></i>';
+                        echo '<i aria-hidden="true" class="far fa-star"></i>';
                     }
                 }
                 echo '</span>';
                 if ($cost > 0) {
                     echo '<span class="market-cost">';
                         if ($market->getPurchase() == 1) {
-                            echo '<i class="fas fa-check-circle"></i>';
+                            echo '<i aria-hidden="true" class="fas fa-check-circle"></i>';
                         } else {
                             if ($cost != $realCost) {
                                 echo '<span style="text-decoration:line-through;">' . $realCost . ' {{€}}</span>';
