cf943c422e8c24e4dba2de289bb7a5562019d1d2
Sylvaner <sylvain.dangin@gmail.com>
2019-11-22 10:44:59 +0100

    Add coverage to sonarcloud

--- tests/tests/libs/tests_funcs.py
+++ tests/tests/libs/tests_funcs.py
@@ -10,6 +10,7 @@ YELLOW = '\033[0;33m'
 BLUE = '\033[0;34m'
 NC = '\033[0m'
 
+
 def print_title(title):
     """Print title with decoration
     :param title: Title to show
@@ -19,11 +20,13 @@ def print_title(title):
     print(BLUE + '*** ' + RED + title + ' ' + BLUE + '***' + NC)
     print(BLUE + ('*' * nb_stars) + NC)
 
+
 def print_subtitle(subtitle):
     """Print subtitle with decoration
     :param subtitle: Subtitle to show
     """
     print('>>> ' + RED + subtitle + NC)
+
 
 def print_info(information_msg):
     """Print information message with decoration
@@ -31,17 +34,20 @@ def print_info(information_msg):
     """
     print('>>>>> ' + GREEN + information_msg + NC)
 
+
 def print_warning(warning_msg):
     """Print warning message with decoration
     :param warning_msg: Warning to show
     """
     print(YELLOW + '/!\\ ' + warning_msg + NC)
 
+
 def print_error(error_msg):
     """Print error message with decoration
     :param error_msg: Error to show
     """
     print(RED + '!!! ' + error_msg + NC)
+
 
 def ask_y_n(question, default='y'):
     """Ask for a question which answer is yes or no
@@ -60,6 +66,7 @@ def ask_y_n(question, default='y'):
         return default
     return choice
 
+
 def get_command_output(command):
     """Execute command and get the ouput
     :param command: Command to execute
@@ -75,15 +82,18 @@ def get_command_output(command):
     status = cmd_process.wait()
     return std_out.decode('utf-8'), status
 
+
 def is_docker_image_initialized():
     """Test if docker image is initialized
     :return: True if the docker image nextdom-test-snap exists
     :rtype:  bool
     """
-    output, status = get_command_output('docker images -q nextdom-test-snap:latest')
+    output, status = get_command_output(
+        'docker images -q nextdom-test-snap:latest')
     if status == 0 and output == '':
         return False
     return True
+
 
 def create_docker():
     """Create docker image for tests
@@ -92,10 +102,12 @@ def create_docker():
     os.system('./scripts/remove_docker.sh')
     os.system('./scripts/prepare_docker.sh')
 
+
 def clear_docker():
     """Remove and kill all containers used for tests
     """
-    containers_to_remove, _ = get_command_output('docker ps -a --filter "name=nextdom-test" -q')
+    containers_to_remove, _ = get_command_output(
+        'docker ps -a --filter "name=nextdom-test" -q')
     containers_list = containers_to_remove.split('\n')
     if len(containers_list) > 1:
         print_info('Clear docker')
@@ -103,6 +115,7 @@ def clear_docker():
             if container_id != '':
                 os.system('docker kill ' + container_id + ' > /dev/null 2>&1')
                 os.system('docker rm ' + container_id + ' > /dev/null 2>&1')
+
 
 def init_docker():
     """Create docker image or ask for reset it. Avoid in travis environment.
@@ -116,6 +129,7 @@ def init_docker():
                 create_docker()
         clear_docker()
 
+
 def start_test_container(container_name, default_password=''):
     """Start a test container
     :param container_name:   Name of the container
@@ -125,9 +139,12 @@ def start_test_container(container_name, default_password=''):
     """
     print_info('Setup')
     if default_password == '':
-        os.system('./scripts/start_test_container.sh nextdom-test-' + container_name)
+        os.system(
+            './scripts/start_test_container.sh nextdom-test-' + container_name)
     else:
-        os.system('./scripts/start_test_container.sh nextdom-test-' + container_name + ' ' + default_password) #pylint: disable=line-too-long
+        os.system('./scripts/start_test_container.sh nextdom-test-' +
+                  container_name + ' ' + default_password)  # pylint: disable=line-too-long
+
 
 def remove_test_container(container_name):
     """Remove a test container
@@ -136,6 +153,7 @@ def remove_test_container(container_name):
     """
     print_info('Clear')
     os.system('./scripts/remove_test_container.sh nextdom-test-' + container_name)
+
 
 def exec_command_in_container(container_name, command):
     """Execute a command a test container
@@ -146,6 +164,7 @@ def exec_command_in_container(container_name, command):
     """
     return os.system('docker exec -i nextdom-test-' + container_name + ' ' + command)
 
+
 def copy_file_in_container(container_name, src, dest):
     """Copy a file in a test container
     :param container_name: Name of the container
@@ -155,7 +174,9 @@ def copy_file_in_container(container_name, src, dest):
     :type src:             str
     :type dest:            str
     """
-    os.system('docker cp ' + src + ' nextdom-test-' + container_name + ':' + dest)
+    os.system('docker cp ' + src + ' nextdom-test-' +
+              container_name + ':' + dest)
+
 
 def copy_file_from_container(container_name, src, dest):
     """Copy a file in a test container
@@ -166,7 +187,9 @@ def copy_file_from_container(container_name, src, dest):
     :type src:             str
     :type dest:            str
     """
-    os.system('docker cp nextdom-test-' + container_name + ':' + src + ' ' + dest)
+    os.system('docker cp nextdom-test-' +
+              container_name + ':' + src + ' ' + dest)
+
 
 def start_all_tests(title, tests_list, use_docker=True):
     """Start all tests
@@ -188,6 +211,7 @@ def start_all_tests(title, tests_list, use_docker=True):
             all_tests_pass = False
     return all_tests_pass
 
+
 def start_specific_test(test_name, tests_list):
     """Start specific test choosed by the user
     :param test_name:  Name of the test
@@ -200,6 +224,7 @@ def start_specific_test(test_name, tests_list):
     else:
         print_error('Tests ' + test_name + ' not found')
 
+
 def run_test(path, parameters=None):
     """Run a test file. Stop script with error on fail.
     :param path:       Path of the test file
@@ -211,7 +236,16 @@ def run_test(path, parameters=None):
     test_cmd = 'python3 -W ignore ' + path
     if parameters is not None:
         test_cmd = test_cmd + ' ' + ' '.join(parameters)
-    std_out, status = get_command_output(test_cmd)
-    print(std_out)
+    cmd_process = subprocess.Popen(test_cmd,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.STDOUT,
+                                   shell=True)
+
+    while cmd_process.poll() is None:
+        line = cmd_process.stdout.readline()
+        if line:
+            print(line.strip().decode('utf-8'))
+    status = cmd_process.returncode
+    print(status)
     if status != 0:
         sys.exit(1)
