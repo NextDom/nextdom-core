c9e0cc8ffbc7dbd9a88e188c398e5906f7d15a6e
MacBook\Luc <sanchezluc@gmail.com>
2020-01-01 11:04:57 +0100

    - Licence and prepare Unit Test

--- src/Ajax/TranslationsAjax.php
+++ src/Ajax/TranslationsAjax.php
@@ -1,7 +1,26 @@
 <?php
 
+/* This file is part of NextDom Software.
+ *
+ * NextDom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NextDom Software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NextDom Software. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @Support <https://www.nextdom.org>
+ * @Email   <admin@nextdom.org>
+ * @Authors/Contributors: Sylvaner, Byackee, cyrilphoenix71, ColonelMoutarde, edgd1er, slobberbone, Astral0, DanoneKiD
+ */
 
-namespace NextDom\Controller\Pages;
+namespace NextDom\Ajax;
 
 use NextDom\Controller\BaseController;
 use NextDom\Helpers\TranslateHelper;
@@ -12,7 +31,7 @@ use Symfony\Component\Translation\Translator;
  * Class TranslationsController
  * @package NextDom\Controller\Pages
  */
-class TranslationsController extends BaseController
+class TranslationsAjax extends BaseAjax
 {
     /**
      * @var Translator Translate tool
