727b2f4241750a0cfe606c6efe8f21b49bd3e8c1
Slobberbone <slobberbone4884@gmail.com>
2020-01-10 00:02:13 +0100

    Change bdd nextdomdev to nextdom

--- install/install_git_dev.sh
+++ install/install_git_dev.sh
@@ -3,15 +3,9 @@ set -e
 
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-source ${CURRENT_DIR}/scripts/utils.sh
-
 #################################################################################################
 ################################### NextDom Installation from git ###############################
 #################################################################################################
-
-if [[! -f ${ROOT_DIRECTORY}/.git  ]] ; then
-    addLogInfo "Il n'y a pas de dossier .git"
-fi
 
 sed -i "s|PRODUCTION=true|PRODUCTION=false|g" ${CURRENT_DIR}/scripts/config.sh
 
