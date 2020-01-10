10d3b1f850e05fc2112cf5725e8ed9a93f3e66a0
Slobberbone <slobberbone4884@gmail.com>
2020-01-10 21:50:38 +0100

    No historical since 2020
    Fix https://github.com/NextDom/nextdom-core/issues/1652

--- src/Managers/HistoryManager.php
+++ src/Managers/HistoryManager.php
@@ -153,9 +153,9 @@ class HistoryManager extends BaseManager
     public static function archive()
     {
         global $NEXTDOM_INTERNAL_CONFIG;
-        $sql = 'DELETE FROM ' . self::DB_CLASS_NAME . ' WHERE `datetime` <= "2000-01-01 01:00:00" OR  `datetime` >= "2020-01-01 01:00:00"';
+        $sql = 'DELETE FROM history WHERE `datetime` <= "2000-01-01 01:00:00"';
         DBHelper::exec($sql);
-        $sql = 'DELETE FROM historyArch WHERE `datetime` <= "2000-01-01 01:00:00" OR  `datetime` >= "2020-01-01 01:00:00"';
+        $sql = 'DELETE FROM historyArch WHERE `datetime` <= "2000-01-01 01:00:00"';
         DBHelper::exec($sql);
         $sql = 'DELETE FROM ' . self::DB_CLASS_NAME . ' WHERE `value` IS NULL';
         DBHelper::exec($sql);
