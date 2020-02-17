02bcee0d75b332bb2889c62505470027f90b1ae7
Sylvain Dangin <sylvain.dangin@gmail.com>
2020-02-17 22:21:35 +0100

    Fix travis chrome install

--- tests/install_chrome.sh
+++ tests/install_chrome.sh
@@ -3,4 +3,5 @@
 export CHROME_SOURCE_URL=https://dl.google.com/dl/linux/direct/google-chrome-stable_current_amd64.deb
 wget --no-verbose -O /tmp/$(basename $CHROME_SOURCE_URL) $CHROME_SOURCE_URL
 dpkg -i /tmp/$(basename $CHROME_SOURCE_URL)
+apt-get update
 apt-get install -yf
